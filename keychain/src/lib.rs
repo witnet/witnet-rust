c83ce513bf9405162269f0a5716f342d617b7118
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-02-09 12:58:02 +0100

    Basic witnet_keychain crate

---
+++ keychain/src/lib.rs
@@ -0,0 +1,27 @@
+//Rust-Witnet is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//Rust-Witnet is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+// along with Rust-Witnet. If not, see <http://www.gnu.org/licenses/>.
+//
+//This file is based on keychain/src/lib.rs from
+// <https://github.com/mimblewimble/grin>,
+// originally developed by The Grin Developers and distributed under the
+// Apache License, Version 2.0. You may obtain a copy of the License at
+// <http://www.apache.org/licenses/LICENSE-2.0>.
+
+//! Library module for the key holder functionalities provided by Rust-Witnet.
+
+extern crate witnet_util as util;
+
+mod extkey;
+
+pub mod keychain;
+pub use keychain::{Error};
