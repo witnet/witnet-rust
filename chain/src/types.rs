830e863f6859b7c059edc16040ae00e9f39e7e02
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2018-02-09 12:57:00 +0100

    Basic witnet_chain crate

---
+++ chain/src/types.rs
@@ -0,0 +1,29 @@
+//This file is part of Rust-Witnet.
+//
+//Rust-Witnet is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//Rust-Witnet is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+// along with Rust-Witnet. If not, see <http://www.gnu.org/licenses/>.
+//
+//This file is based on chain/src/types.rs from
+// <https://github.com/mimblewimble/grin>,
+// originally developed by The Grin Developers and distributed under the
+// Apache License, Version 2.0. You may obtain a copy of the License at
+// <http://www.apache.org/licenses/LICENSE-2.0>.
+
+//! Base types that the block chain pipeline requires.
+
+/// Errors thrown by chain validation.
+#[derive(Debug)]
+pub enum Error {
+    // TODO Stablish all possible chain errors
+    Other(String),
+}
