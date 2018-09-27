e522f9569c519f63396d00c7a274b310dcf46421
csmoe <35686186+csmoe@users.noreply.github.com>
2018-09-27 21:27:45 +0800

    deny missing docs for public api

--- data_structures/src/lib.rs
+++ data_structures/src/lib.rs
@@ -1,3 +1,12 @@
+//! data structures
+
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(missing_docs)]
+
+/// data greeting
 pub fn greetings() -> String {
     println!("Hello from data structures!");
     String::from("Hello from data structures!")
--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,3 +1,12 @@
+//! storage
+
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(missing_docs)]
+
+/// storage greeting
 pub fn greetings() -> String {
     println!("Hello from storage!");
     String::from("Hello from storage!")
