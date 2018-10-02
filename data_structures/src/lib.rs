2443bf81ca62ff1abf1fbddbc08dd331a269fee0
Mario Cao <mario@stampery.co>
2018-10-02 12:45:41 +0200

    refactor(data-fbs): refactor mod + doc-test
    This commit includes the following:
     - Refactor: folder structure
     - Fix: attributes for flatbuffers mod
     - Test: doc-test example

--- data_structures/src/lib.rs
+++ data_structures/src/lib.rs
@@ -1,12 +1,7 @@
 //! data structures
 
-// Removed due to flatbuffers generated code
-// #![deny(rust_2018_idioms)]
-#![deny(non_upper_case_globals)]
-#![deny(non_camel_case_types)]
-#![deny(non_snake_case)]
-#![deny(unused_mut)]
-#![deny(missing_docs)]
+// To enable `#[allow(clippy::all)]`
+#![feature(tool_lints)]
 
 /// data greeting
 pub fn greetings() -> String {
@@ -15,5 +10,4 @@ pub fn greetings() -> String {
 }
 
 /// Generated Message module from Flatbuffers compiler
-#[allow(missing_docs)]
-pub mod protocol_generated;
+pub mod flatbuffers;
