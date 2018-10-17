59edb6e410ce42e46738744fb1397bd272fe912c
Mario Cao <mario@stampery.co>
2018-10-17 16:46:41 +0200

    tests(p2p): remove p2p greetings tests

--- p2p/tests/lib.rs
+++ p2p/tests/lib.rs
@@ -1,8 +1,2 @@
 use witnet_p2p as p2p;
 
-use crate::p2p::greetings;
-
-#[test]
-fn p2p_greeeting() {
-    assert_eq!(greetings(), String::from("Hello from p2p!"));
-}
--- tests/witnet.rs
+++ tests/witnet.rs
@@ -1,8 +1,6 @@
 use witnet_crypto as crypto;
-use witnet_p2p as p2p;
 
 #[test]
 fn greetings() {
     assert_eq!(crypto::greetings(), String::from("Hello from crypto!"));
-    assert_eq!(p2p::greetings(), String::from("Hello from p2p!"));
 }
