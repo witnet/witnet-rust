0e2342c268d6c8f725953fcb094a1da5783c5d8a
mmartinbar <maria@stampery.co>
2018-10-24 15:33:13 +0200

    feat(core): remove client and server actors from core
    Their functionality will be implemented by connections manager

--- core/src/actors/mod.rs
+++ core/src/actors/mod.rs
@@ -4,14 +4,8 @@ pub mod codec;
 /// Module running system actor
 pub mod node;
 
-/// Client actor module
-pub mod client;
-
 /// Peers manager actor module
 pub mod peers_manager;
-
-/// Server actor module
-pub mod server;
 
 /// Session manager actor module
 pub mod session_manager;
