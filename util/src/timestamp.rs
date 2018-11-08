3fd0df89567b2e1052bc2bdb046b9b2e3f65ffc0
mmartinbar <maria@stampery.co>
2018-11-08 11:46:05 +0100

    refactor(util): modify get_timestamp function to use chrono crate

--- util/src/timestamp.rs
+++ util/src/timestamp.rs
@@ -1,12 +1,10 @@
-use std::time::{SystemTime, SystemTimeError, UNIX_EPOCH};
+use chrono::prelude::*;
 
-use crate::error::{WitnetError, WitnetResult};
+/// Function to get timestamp from system as UTC Unix timestamp, seconds since Unix epoch
+pub fn get_timestamp() -> i64 {
+    // Get UTC current datetime
+    let utc: DateTime<Utc> = Utc::now();
 
-/// Function to get timestamp from system as UTC
-pub fn get_timestamp() -> WitnetResult<u64, SystemTimeError> {
-    // Get timestamp from system
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(value) => Ok(value.as_secs()),
-        Err(e) => Err(WitnetError::from(e)),
-    }
+    // Return number of non-leap seconds since Unix epoch
+    utc.timestamp()
 }
