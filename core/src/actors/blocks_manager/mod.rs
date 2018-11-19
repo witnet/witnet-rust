d765cefae7a509fd09e671865b8f6263b462f186
mmartinbar <maria@stampery.co>
2018-11-19 18:11:11 +0100

    docs(managers): add missing doc for blocks manager
    From Anler's 6c94f6d0b0f402e0c937a027bc2f845cbdfd581d commit

--- core/src/actors/blocks_manager/mod.rs
+++ core/src/actors/blocks_manager/mod.rs
@@ -1,3 +1,17 @@
+//! # Blocks Manager actor
+//!
+//! This module contains the Blocks Manager actor which is in charge
+//! of managing the blocks of the Witnet blockchain received through
+//! the protocol. Among its responsabilities lie the following:
+//!
+//! * Initializing the chain info upon running the node for the first time and persisting it into storage [StorageManager](actors::storage_manager::StorageManager)
+//! * Recovering the chain info from storage and keeping it in its state.
+//! * Validating block candidates as they come from a session
+//! * Consolidating multiple block candidates for the same checkpoint into a single valid block.
+//! * Putting valid blocks into storage by sending them to the storage manager actor.
+//! * Having a method for letting other components to get blocks by *hash* or *checkpoint*.
+//! * Having a method for letting other components to get the epoch of the current tip of the blockchain (e.g. last epoch field required for the handshake in the Witnet network protocol)
+
 use actix::{Supervised, SystemService};
 
 mod actor;
--- docs/architecture/managers/blocks-manager.md
+++ docs/architecture/managers/blocks-manager.md
@@ -1,7 +1,14 @@
 # Blocks Manager
 
-The __blocks manager__ is the actor in charge of managing the blocks of Witnet blockchain in the
-node.
+The __blocks manager__ is the actor in charge of managing the blocks of the Witnet blockchain. Among its responsabilities lie the following:
+
+* Initializing the chain info upon running the node for the first time and persisting it into storage (see **Storage Manager**).
+* Recovering the chain info from storage and keeping it in its state.
+* Validating block candidates as they come from a session (see **Sessions Manager**).
+* Consolidating multiple block candidates for the same checkpoint into a single valid block.
+* Putting valid blocks into storage by sending them to the storage manager actor.
+* Having a method for letting other components to get blocks by *hash* or *checkpoint*.
+* Having a method for letting other components to get the epoch of the current tip of the blockchain (e.g. last epoch field required for the handshake in the Witnet network protocol)
 
 ## State
 
