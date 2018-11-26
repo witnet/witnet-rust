002a3c9f85c3b59222bb0586147aad146d36903f
Luis Rubio <luisr@stampery.co>
2018-11-26 20:43:11 +0100

    feat(data-structures): add builder function for get_data messages

--- data_structures/src/builders.rs
+++ data_structures/src/builders.rs
@@ -6,7 +6,8 @@ use rand::{thread_rng, Rng};
 use crate::chain::{Block, BlockHeaderWithProof, InvElem, Transaction};
 
 use crate::types::{
-    Address, Command, GetPeers, Inv, IpAddress, Message, Peers, Ping, Pong, Verack, Version,
+    Address, Command, GetData, GetPeers, Inv, IpAddress, Message, Peers, Ping, Pong, Verack,
+    Version,
 };
 
 use witnet_util::timestamp::get_timestamp;
@@ -99,6 +100,13 @@ impl Message {
     /// Function to build Inv messages
     pub fn build_inv(inv_elems: Vec<InvElem>) -> Message {
         Message::build_message(Command::Inv(Inv {
+            inventory: inv_elems,
+        }))
+    }
+
+    /// Function to build GetData messages
+    pub fn build_get_data(inv_elems: Vec<InvElem>) -> Message {
+        Message::build_message(Command::GetData(GetData {
             inventory: inv_elems,
         }))
     }
