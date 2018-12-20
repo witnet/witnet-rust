a04991ee666de9ccbc63dfb7e4a17be10b561267
Mario Cao <mario@stampery.co>
2018-12-20 18:12:52 +0100

    refactor(util): add get timestamp with nanos

--- util/src/timestamp.rs
+++ util/src/timestamp.rs
@@ -8,3 +8,12 @@ pub fn get_timestamp() -> i64 {
     // Return number of non-leap seconds since Unix epoch
     utc.timestamp()
 }
+
+/// Function to get timestamp from system as UTC Unix timestamp, seconds and nanoseconds since Unix epoch
+pub fn get_timestamp_nanos() -> (i64, u32) {
+    // Get UTC current datetime
+    let utc: DateTime<Utc> = Utc::now();
+
+    // Return number of non-leap seconds since Unix epoch and the number of nanoseconds since the last second boundary
+    (utc.timestamp(), utc.timestamp_subsec_nanos())
+}
