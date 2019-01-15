7fc052679490269f408cbcd8b026466cb36a235f
Adán SDPC <adan@witnet.foundation>
2019-01-15 12:25:29 +0100

    feat(rad): add witnet_rad::operators module

---
+++ rad/src/operators/mod.rs
@@ -0,0 +1,76 @@
+// FIXME: https://github.com/rust-num/num-derive/issues/20
+#![allow(clippy::useless_attribute)]
+
+use crate::error::RadResult;
+use crate::script::RadonCall;
+use crate::types::RadonTypes;
+
+use num_derive::FromPrimitive;
+use std::fmt;
+
+pub mod mixed;
+pub mod string;
+
+#[derive(Debug, FromPrimitive, PartialEq)]
+pub enum RadonOpCodes {
+    /// Only for the sake of allowing catch-alls when matching
+    Fail = -1,
+    // Multi-type operator codes start at 0x00
+    /// Identity operator code
+    Identity = 0x00,
+    // Boolean operator codes start at 0x10
+    // Integer operator codes start at 0x20
+    // Float operator codes start at 0x30
+    // Null operator codes start at 0x40
+    // String operator codes start at 0x50
+    /// Parse Mixed from JSON string
+    ParseJson = 0x53,
+    // Array operator codes start at 0x60
+    // Map operator codes start at 0x70
+    // Mixed operator codes start at 0x80
+    // Result operator codes start at 0x90
+}
+
+impl fmt::Display for RadonOpCodes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "RadonOpCodes::{:?}", self)
+    }
+}
+
+pub trait Operable<'a> {
+    fn operate(self, call: &RadonCall) -> RadResult<RadonTypes<'a>>;
+}
+
+pub fn operate<'a>(input: RadonTypes<'a>, call: &'a RadonCall) -> RadResult<RadonTypes<'a>> {
+    match input {
+        RadonTypes::String(radon_string) => radon_string.operate(call),
+        RadonTypes::Mixed(radon_mixed) => radon_mixed.operate(call),
+    }
+}
+
+pub fn identity(input: RadonTypes) -> RadResult<RadonTypes> {
+    Ok(input)
+}
+
+#[test]
+pub fn test_identity() {
+    use crate::types::string::RadonString;
+
+    let input = RadonString::from("Hello world!").into();
+    let expected = RadonString::from("Hello world!").into();
+    let output = identity(input).unwrap();
+
+    assert_eq!(output, expected);
+}
+
+#[test]
+pub fn test_operate() {
+    use crate::types::string::RadonString;
+
+    let input = RadonString::from("Hello world!").into();
+    let expected = RadonString::from("Hello world!").into();
+    let call = (RadonOpCodes::Identity, None);
+    let output = operate(input, &call).unwrap();
+
+    assert_eq!(output, expected);
+}
---
+++ rad/src/operators/string.rs
@@ -0,0 +1,72 @@
+use crate::error::*;
+use crate::types::{mixed::RadonMixed, string::RadonString, RadonType};
+
+use json;
+use rmpv;
+use std::{error::Error, str};
+
+pub fn parse_json(input: &RadonString) -> RadResult<RadonMixed> {
+    match str::from_utf8(input.value()) {
+        Ok(string) => match json::parse(string) {
+            Ok(json_value) => {
+                let value = json_to_rmp(&json_value);
+                Ok(RadonMixed::from(value.to_owned()))
+            }
+            Err(json_error) => Err(WitnetError::from(RadError::new(
+                RadErrorKind::JsonParse,
+                json_error.description().to_owned(),
+            ))),
+        },
+        Err(_utf8_error) => Err(WitnetError::from(RadError::new(
+            RadErrorKind::JsonParse,
+            String::from("Failed to parse an object from a JSON buffer"),
+        ))),
+    }
+}
+
+fn json_to_rmp(value: &json::JsonValue) -> rmpv::ValueRef {
+    match value {
+        json::JsonValue::Array(value) => {
+            rmpv::ValueRef::Array(value.into_iter().map(json_to_rmp).collect())
+        }
+        json::JsonValue::Object(value) => {
+            let entries = value
+                .iter()
+                .map(|(key, value)| (rmpv::ValueRef::from(key), json_to_rmp(value)))
+                .collect();
+            rmpv::ValueRef::Map(entries)
+        }
+        json::JsonValue::Short(value) => {
+            rmpv::ValueRef::String(rmpv::Utf8StringRef::from(value.as_str()))
+        }
+        json::JsonValue::String(value) => {
+            rmpv::ValueRef::String(rmpv::Utf8StringRef::from(value.as_str()))
+        }
+        _ => rmpv::ValueRef::Nil,
+    }
+}
+
+#[test]
+fn test_parse_json() {
+    let valid_string = RadonString::from(r#"{ "Hello": "world" }"#);
+    let invalid_string = RadonString::from(r#"{ "Hello": }"#);
+
+    let valid_object = parse_json(&valid_string).unwrap();
+    let invalid_object = parse_json(&invalid_string);
+
+    assert!(if let rmpv::Value::Map(vector) = valid_object.value() {
+        if let Some((rmpv::Value::String(key), rmpv::Value::String(val))) = vector.first() {
+            key.as_str() == Some("Hello") && val.as_str() == Some("world")
+        } else {
+            false
+        }
+    } else {
+        false
+    });
+
+    assert!(if let Err(_error) = invalid_object {
+        true
+    } else {
+        false
+    });
+}
