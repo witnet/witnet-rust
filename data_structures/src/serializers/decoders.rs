1092e24049787053f44002f8672de4481b898d89
Adán SDPC <adan@witnet.foundation>
2019-01-17 19:41:52 +0100

    feat(data_structures): implement TryInto trait

--- data_structures/src/serializers/decoders.rs
+++ data_structures/src/serializers/decoders.rs
@@ -95,6 +95,23 @@ pub trait TryFrom<T>: Sized {
     fn try_from(value: T) -> Result<Self, Self::Error>;
 }
 
+pub trait TryInto<T: Sized> {
+    type Error;
+
+    fn try_into(self) -> Result<T, Self::Error>;
+}
+
+impl<A, B> TryInto<B> for A
+where
+    B: TryFrom<A>,
+{
+    type Error = B::Error;
+
+    fn try_into(self) -> Result<B, Self::Error> {
+        B::try_from(self)
+    }
+}
+
 impl TryFrom<Vec<u8>> for Message {
     type Error = &'static str;
 
