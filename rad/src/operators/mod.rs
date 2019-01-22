602e3e0845397a7fd62b7017e0b956bf55be7d4f
Adán SDPC <adan@witnet.foundation>
2019-01-22 15:23:18 +0100

    feat(rad): add support for RadonArray

--- rad/src/operators/mod.rs
+++ rad/src/operators/mod.rs
@@ -37,14 +37,15 @@ impl fmt::Display for RadonOpCodes {
     }
 }
 
-pub trait Operable<'a> {
-    fn operate(self, call: &RadonCall) -> RadResult<RadonTypes<'a>>;
+pub trait Operable {
+    fn operate(self, call: &RadonCall) -> RadResult<RadonTypes>;
 }
 
-pub fn operate<'a>(input: RadonTypes<'a>, call: &'a RadonCall) -> RadResult<RadonTypes<'a>> {
+pub fn operate(input: RadonTypes, call: &RadonCall) -> RadResult<RadonTypes> {
     match input {
         RadonTypes::String(radon_string) => radon_string.operate(call),
         RadonTypes::Mixed(radon_mixed) => radon_mixed.operate(call),
+        RadonTypes::Array(radon_array) => radon_array.operate(call),
     }
 }
 
