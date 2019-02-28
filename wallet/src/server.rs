b67778a80d5673095fb3a2f4d4b98783daaf0c31
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2019-02-28 13:01:37 +0100

    feat(wallet): create mocked API
    Co-authored-by: Tommytrg <tomastrg22@gmail.com>

--- wallet/src/server.rs
+++ wallet/src/server.rs
@@ -4,7 +4,7 @@ use actix::{Actor, ActorFuture, Arbiter, Context, Handler, Message, ResponseActF
 use futures::future::Future;
 use jsonrpc_ws_server::jsonrpc_core::{IoHandler, Params, Value};
 use jsonrpc_ws_server::{Server, ServerBuilder};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use std::net::SocketAddr;
 use async_jsonrpc_client::Transport;
 use async_jsonrpc_client::transports::tcp::TcpSocket;
@@ -41,9 +41,426 @@ fn start_ws_jsonrpc_server(
     io.add_method("getOutput", move |params: Params| {
         forward_call("getOutput",&reg, params.parse())
     });
+    let reg = registry.clone();
+    io.add_method("getWalletInfos", move |params: Params| {
+        get_wallet_infos(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("createMnemonics", move |params: Params| {
+        create_mnemonics(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("importSeed", move |params: Params| {
+        import_seed(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("createWallet", move |params: Params| {
+        create_wallet(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("unlockWallet", move |params: Params| {
+        unlock_wallet(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("getTransactions", move |params: Params| {
+        get_transactions(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("sendVTT", move |params: Params| {
+        send_vtt(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("generateAddress", move |params: Params| {
+        generate_address(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("createDataRequest", move |params: Params| {
+        create_data_request(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("runDataRequest", move |params: Params| {
+        run_data_request(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("sendDataRequest", move |params: Params| {
+        send_data_request(&reg, params.parse())
+    });
+    let reg = registry.clone();
+    io.add_method("lockWallet", move |params: Params| {
+        lock_wallet(&reg, params.parse())
+    });
 
     // Start the WebSockets JSON-RPC server in a new thread and bind to address "addr"
     ServerBuilder::new(io).start(addr)
+}
+
+#[derive(Deserialize)]
+struct LockWalletParams {
+    wallet_id: String,
+    #[serde(default)] // default to false
+    wipe: bool,
+}
+
+fn lock_wallet(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<LockWalletParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = true;
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+fn send_data_request(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<DataRequest>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = RadonValue{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+// TODO: radon crate
+#[derive(Serialize)]
+struct RadonValue {}
+
+fn run_data_request(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<DataRequest>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = RadonValue{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateDataRequestParams {
+    not_before: u64,
+    retrieve: Vec<RADRetrieveArgs>,
+    aggregate: RADAggregateArgs,
+    consensus: RADConsensusArgs,
+    deliver: Vec<RADDeliverArgs>,
+}
+
+// TODO: radon crate
+#[derive(Debug, Deserialize, Serialize)]
+struct RADType(String);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RADRetrieveArgs {
+    kind: RADType,
+    url: String,
+    script: Vec<Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RADAggregateArgs {
+    script: Vec<Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RADConsensusArgs {
+    script: Vec<Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RADDeliverArgs {
+    kind: RADType,
+    url: String,
+}
+
+// TODO: data_structures crate
+#[derive(Debug, Deserialize, Serialize)]
+struct DataRequest {}
+
+fn create_data_request(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<CreateDataRequestParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = DataRequest{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+// TODO: is this pkh?
+#[derive(Serialize)]
+struct Address {}
+
+fn generate_address(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<()>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = Address{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Deserialize)]
+struct SendVttParams {
+    wallet_id: String,
+    to_address: Vec<u8>,
+    amount: u64,
+    fee: u64,
+    subject: String,
+}
+
+fn send_vtt(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<SendVttParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = Transaction{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Deserialize)]
+struct GetTransactionsParams {
+    wallet_id: String,
+    limit: u32,
+    page: u32,
+}
+
+// TODO: import data_structures crate
+#[derive(Serialize)]
+struct Transaction {}
+
+fn get_transactions(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<GetTransactionsParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x: Vec<Transaction> = vec![];
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct UnlockWalletParams {
+    id: String,
+    password: String,
+}
+
+fn unlock_wallet(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<UnlockWalletParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = Wallet::for_test();
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Wallet {
+    version: u32,
+    info: WalletInfo,
+    seed: SeedInfo,
+    epochs: EpochsInfo,
+    purpose: DerivationPath,
+    accounts: Vec<Account>,
+}
+
+impl Wallet {
+    fn for_test() -> Self {
+        Wallet {
+            version: 0,
+            info: WalletInfo { id: "".to_string(), caption: "".to_string() },
+            seed: SeedInfo::Wip3(Seed(vec![])),
+            epochs: EpochsInfo { last: 0, born: 0 },
+            purpose: DerivationPath(format!("m/44'/60'/0'/0")),
+            accounts: vec![]
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+enum SeedInfo {
+    Wip3(Seed),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Seed(Vec<u8>);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EpochsInfo {
+    last: u32,
+    born: u32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct DerivationPath(String);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Account {
+    key_path: KeyPath,
+    key_chains: Vec<KeyChain>,
+    balance: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct KeyPath(Vec<ChildNumber>);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ChildNumber(u32);
+
+#[derive(Debug, Deserialize, Serialize)]
+enum KeyChain {
+    ExternalKeyChain,
+    InternalKeyChain,
+    RadKeyChain,
+}
+
+#[derive(Deserialize)]
+struct CreateWalletParams {
+    name: String,
+    password: String,
+}
+
+fn create_wallet(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<CreateWalletParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = Wallet::for_test();
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum ImportSeedParams {
+    Mnemonics { mnemonics: Mnemonics },
+    Seed { seed: String },
+}
+
+fn import_seed(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<ImportSeedParams>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = true;
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+// TODO: implemented in PR #432
+#[derive(Debug, Deserialize, Serialize)]
+struct Mnemonics {}
+
+fn create_mnemonics(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<()>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x = Mnemonics{};
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
+}
+
+
+#[derive(Debug, Deserialize, Serialize)]
+struct WalletInfo {
+    id: String,
+    caption: String,
+}
+
+fn get_wallet_infos(
+    registry: &SystemRegistry,
+    params: jsonrpc_ws_server::jsonrpc_core::Result<()>,
+) -> impl Future<Item = Value, Error = jsonrpc_ws_server::jsonrpc_core::Error> {
+    let params = match params {
+        Ok(x) => x,
+        Err(e) => return Box::new(futures::failed(e))
+    };
+
+    let x: Vec<WalletInfo> = vec![];
+    Box::new(futures::done(serde_json::to_value(x).map_err(|e| {
+        let mut err = jsonrpc_ws_server::jsonrpc_core::Error::internal_error();
+        err.message = e.to_string();
+        err
+    })))
 }
 
 /// Forwards a JSON-RPC call to the node
@@ -73,7 +490,7 @@ fn forward_call(method: &str, registry: &SystemRegistry, params: jsonrpc_ws_serv
 }
 
 /// Example JSON-RPC method parameters
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct SayHelloParams {
     name: String,
 }
