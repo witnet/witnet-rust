2cdd836dd689fc29bcaddc29fd30f621b7831e1c
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2019-04-23 19:17:53 +0200

    feat(reputation): create reputation crate and TRS

---
+++ reputation/src/lib.rs
@@ -0,0 +1,11 @@
+//! Reputation engine
+
+#![deny(rust_2018_idioms)]
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(missing_docs)]
+
+pub mod trs;
+pub use trs::TotalReputationSet;
