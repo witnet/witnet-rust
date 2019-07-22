aa1b10b4850acc8e6146a3851e9332e7b3ac40fb
Luis Rubio <luisr@stampery.co>
2019-07-22 18:05:06 +0200

    test(crypto): add test to check der and compact functions

--- crypto/src/signature.rs
+++ crypto/src/signature.rs
@@ -32,7 +32,7 @@ pub fn verify(public_key: &PublicKey, data: &[u8], sig: &Signature) -> Result<()
 #[cfg(test)]
 mod tests {
     use crate::signature::{sign, verify};
-    use secp256k1::{PublicKey, Secp256k1, SecretKey};
+    use secp256k1::{PublicKey, Secp256k1, SecretKey, Signature};
 
     #[test]
     fn test_sign_and_verify() {
@@ -51,5 +51,49 @@ mod tests {
         assert_eq!(signature_expected.to_string(), signature.to_string());
 
         assert!(verify(&public_key, &data, &signature).is_ok());
+    }
+
+    #[test]
+    fn test_der_and_compact() {
+        let der1 = "3044\
+                    0220\
+                    3dc4fa74655c21b7ffc0740e29bfd88647e8dfe2b68c507cf96264e4e7439c1f\
+                    0220\
+                    7aa61261b18eebdfdb704ca7bab4c7bcf7961ae0ade5309f6f1398e21aec0f9f";
+        let signature1 = Signature::from_der(hex::decode(der1).unwrap().as_slice()).unwrap();
+
+        let r_s = signature1.serialize_compact();
+        let (r, s) = r_s.split_at(32);
+
+        let r_expected =
+            hex::decode("3dc4fa74655c21b7ffc0740e29bfd88647e8dfe2b68c507cf96264e4e7439c1f")
+                .unwrap();
+        let s_expected =
+            hex::decode("7aa61261b18eebdfdb704ca7bab4c7bcf7961ae0ade5309f6f1398e21aec0f9f")
+                .unwrap();
+
+        assert_eq!(r.to_vec(), r_expected);
+        assert_eq!(s.to_vec(), s_expected);
+
+        let der2 = "3045\
+                    0220\
+                    397116930c282d1fcb71166a2d06728120cf2ee5cf6ccd4e2d822e8e0ae24a30\
+                    0221\
+                    009e997d4718a7603942834fbdd22a4b856fc4083704ede62033cf1a77cb9822a9";
+
+        let signature2 = Signature::from_der(hex::decode(der2).unwrap().as_slice()).unwrap();
+
+        let r_s = signature2.serialize_compact();
+        let (r, s) = r_s.split_at(32);
+
+        let r_expected =
+            hex::decode("397116930c282d1fcb71166a2d06728120cf2ee5cf6ccd4e2d822e8e0ae24a30")
+                .unwrap();
+        let s_expected =
+            hex::decode("9e997d4718a7603942834fbdd22a4b856fc4083704ede62033cf1a77cb9822a9")
+                .unwrap();
+
+        assert_eq!(r.to_vec(), r_expected);
+        assert_eq!(s.to_vec(), s_expected);
     }
 }
