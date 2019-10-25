6ecfb7cd5ae6d1993adc382c5a4e9bb045912a33
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2019-10-25 12:24:14 +0200

    chore: replace rust-crypto with aes crate

--- crypto/src/cipher.rs
+++ crypto/src/cipher.rs
@@ -1,82 +1,43 @@
 //! Cipher
-use crypto::{
-    aes,
-    blockmodes::PkcsPadding,
-    buffer::{self, ReadBuffer, WriteBuffer},
-    symmetriccipher,
-};
+use aes::Aes256;
+//use aes_soft as aes;
+use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
 use failure::Fail;
 use rand::{rngs::OsRng, RngCore};
+
+// create an alias for convenience
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 
 /// Error that can be raised when encrypting/decrypting
 #[derive(Debug, Fail)]
 pub enum Error {
-    /// Wrapper for cipher errors
-    #[fail(display = "Encryption/decryption error")]
-    Cipher(symmetriccipher::SymmetricCipherError),
+    /// Block mode error
+    #[fail(display = "{}", _0)]
+    BlockModeError(block_modes::BlockModeError),
+    /// Invalid key IV length
+    #[fail(display = "{}", _0)]
+    InvalidKeyIvLength(block_modes::InvalidKeyIvLength),
     /// Wrapper for random generation errors
     #[fail(display = "Random generation error")]
     Rng(rand::Error),
 }
 
 /// Encrypt data with AES CBC using the supplied secret
-pub fn encrypt_aes_cbc(secret: &[u8], data: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
-    let mut encryptor = aes::cbc_encryptor(aes::KeySize::KeySize256, secret, iv, PkcsPadding);
-    let mut final_result = Vec::<u8>::new();
-    let mut read_buffer = buffer::RefReadBuffer::new(data);
-    let mut buffer = [0; 4096];
-    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+pub fn encrypt_aes_cbc(secret: &[u8], plaintext: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
+    let cipher = Aes256Cbc::new_var(&secret, &iv).map_err(Error::InvalidKeyIvLength)?;
+    let ciphertext = cipher.encrypt_vec(plaintext);
 
-    loop {
-        let result = encryptor
-            .encrypt(&mut read_buffer, &mut write_buffer, true)
-            .map_err(Error::Cipher)?;
-
-        final_result.extend(
-            write_buffer
-                .take_read_buffer()
-                .take_remaining()
-                .iter()
-                .cloned(),
-        );
-
-        match result {
-            buffer::BufferResult::BufferUnderflow => break,
-            buffer::BufferResult::BufferOverflow => (),
-        }
-    }
-
-    Ok(final_result)
+    Ok(ciphertext)
 }
 
 /// Decrypt data with AES CBC using the supplied secret
-pub fn decrypt_aes_cbc(secret: &[u8], data: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
-    let mut decryptor = aes::cbc_decryptor(aes::KeySize::KeySize256, secret, iv, PkcsPadding);
-    let mut final_result = Vec::<u8>::new();
-    let mut read_buffer = buffer::RefReadBuffer::new(data);
-    let mut buffer = [0; 4096];
-    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+pub fn decrypt_aes_cbc(secret: &[u8], ciphertext: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
+    let cipher = Aes256Cbc::new_var(&secret, &iv).map_err(Error::InvalidKeyIvLength)?;
+    let plaintext = cipher
+        .decrypt_vec(ciphertext)
+        .map_err(Error::BlockModeError)?;
 
-    loop {
-        let result = decryptor
-            .decrypt(&mut read_buffer, &mut write_buffer, true)
-            .map_err(Error::Cipher)?;
-
-        final_result.extend(
-            write_buffer
-                .take_read_buffer()
-                .take_remaining()
-                .iter()
-                .cloned(),
-        );
-
-        match result {
-            buffer::BufferResult::BufferUnderflow => break,
-            buffer::BufferResult::BufferOverflow => (),
-        }
-    }
-
-    Ok(final_result)
+    Ok(plaintext)
 }
 
 /// Generate a random initialization vector of the given size in bytes
