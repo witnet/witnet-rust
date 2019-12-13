808e42d4851595a9a3df1fea64af1dec94ecc1dd
Luis Rubio <luisr@stampery.co>
2019-12-13 11:37:55 +0100

    refactor(rad): modify RADON operator codes to the new specifications

--- rad/src/operators/mod.rs
+++ rad/src/operators/mod.rs
@@ -23,100 +23,120 @@ pub mod string;
 pub enum RadonOpCodes {
     /// Only for the sake of allowing catch-alls when matching
     Fail = 0xFF,
-    // Multi-type operator codes start at 0x00
-    /// Identity operator code
-    Identity = 0x00,
-    //Equals = 0x01,
-    /// Array::get, Map::get, Result::get
-    Get = 0x01,
-
     ///////////////////////////////////////////////////////////////////////
-    // Boolean operator codes (start at 0x10)
-    //    BooleanMatch = 0x10,
-    BooleanNegate = 0x11,
-    BooleanAsString = 0x12,
-
-    // Integer operator codes (start at 0x20)
-    IntegerAbsolute = 0x20,
-    IntegerAsMixed = 0x21,
-    IntegerAsFloat = 0x22,
-    IntegerAsString = 0x23,
-    IntegerGreaterThan = 0x24,
-    IntegerLessThan = 0x25,
-    //    IntegerMatch = 0x26,
-    IntegerModulo = 0x27,
-    IntegerMultiply = 0x28,
-    IntegerNegate = 0x29,
-    IntegerPower = 0x2A,
-    //    IntegerReciprocal = 0x2B,
-    //    IntegerSum = 0x2C,
-
-    // Float operator codes (start at 0x30)
-    FloatAbsolute = 0x30,
-    FloatAsMixed = 0x31,
-    FloatAsString = 0x32,
-    FloatCeiling = 0x33,
-    FloatGreaterThan = 0x34,
-    FloatFloor = 0x35,
-    FloatLessThan = 0x36,
-    FloatModulo = 0x37,
-    FloatMultiply = 0x38,
-    FloatNegate = 0x39,
-    FloatPower = 0x3A,
-    //    FloatReciprocal = 0x3B,
-    FloatRound = 0x3C,
-    //    FloatSum = 0x3D,
-    FloatTruncate = 0x3E,
-
-    // String operator codes (start at 0x40)
-    StringAsMixed = 0x40,
-    StringAsFloat = 0x41,
-    StringAsInteger = 0x42,
-    StringLength = 0x43,
-    StringMatch = 0x44,
-    /// Parse Bytes from JSON string
-    StringParseJSON = 0x45,
-    //    StringParseXML = 0x46,
-    StringAsBoolean = 0x47,
-    StringToLowerCase = 0x48,
-    StringToUpperCase = 0x49,
-
-    // Array operator codes (start at 0x50)
-    //    ArrayAsMixed = 0x50,
-    ArrayCount = 0x51,
-    //    ArrayEvery = 0x52,
-    ArrayFilter = 0x53,
-    //    ArrayFlatten = 0x54,
-    ArrayGet = 0x55,
-    ArrayMap = 0x56,
-    ArrayReduce = 0x57,
-    //    ArraySome = 0x58,
-    ArraySort = 0x59,
-    //    ArrayTake = 0x5A,
-
+    // Multi-type operator codes start at 0x00
+    Identity = 0x00,
+    ///////////////////////////////////////////////////////////////////////
+    // Array operator codes (start at 0x10)
+    ArrayCount = 0x10,
+    ArrayFilter = 0x11,
+    //    ArrayFlatten = 0x12,
+    //    ArrayGetArray = 0x13,
+    //    ArrayGetBoolean = 0x14,
+    //    ArrayGetBytes = 0x15,
+    //    ArrayGetFloat = 0x16,
+    //    ArrayGetInteger = 0x17,
+    //    ArrayGetMap = 0x18,
+    //    ArrayGetString = 0x19,
+    ArrayMap = 0x1A,
+    ArrayReduce = 0x1B,
+    //    ArraySome = 0x1C,
+    ArraySort = 0x1D,
+    //    ArrayTake = 0x1E,
+    ///////////////////////////////////////////////////////////////////////
+    // Boolean operator codes (start at 0x20)
+    //    BooleanMatch = 0x20,
+    BooleanNegate = 0x21,
+    ///////////////////////////////////////////////////////////////////////
+    // Bytes operator codes (start at 0x30)
+    BytesAsString = 0x30,
+    BytesHash = 0x31,
+    ///////////////////////////////////////////////////////////////////////
+    // Integer operator codes (start at 0x40)
+    IntegerAbsolute = 0x40,
+    IntegerAsFloat = 0x41,
+    IntegerAsString = 0x42,
+    IntegerGreaterThan = 0x43,
+    IntegerLessThan = 0x44,
+    //    IntegerMatch = 0x45,
+    IntegerModulo = 0x46,
+    IntegerMultiply = 0x47,
+    IntegerNegate = 0x48,
+    IntegerPower = 0x49,
+    //    IntegerReciprocal = 0x4A,
+    //    IntegerSum = 0x4B,
+    ///////////////////////////////////////////////////////////////////////
+    // Float operator codes (start at 0x50)
+    FloatAbsolute = 0x50,
+    FloatAsString = 0x51,
+    FloatCeiling = 0x52,
+    FloatGreaterThan = 0x53,
+    FloatFloor = 0x54,
+    FloatLessThan = 0x55,
+    FloatModulo = 0x56,
+    FloatMultiply = 0x57,
+    FloatNegate = 0x58,
+    FloatPower = 0x59,
+    //    FloatReciprocal = 0x5A,
+    FloatRound = 0x5B,
+    //    FloatSum = 0x5C,
+    FloatTruncate = 0x5D,
+    ///////////////////////////////////////////////////////////////////////
     // Map operator codes (start at 0x60)
     //    MapEntries = 0x60,
-    MapGet = 0x61,
-    MapKeys = 0x62,
+    //    MapGetArray = 0x61,
+    //    MapGetBoolean = 0x62,
+    //    MapGetBytes = 0x63,
+    //    MapGetInteger = 0x64,
+    //    MapGetFloat = 0x65,
+    //    MapGetMap = 0x66,
+    //    MapGetString = 0x67,
+    MapKeys = 0x68,
+    //    MapValuesArray = 0x69,
+    //    MapValuesBoolean = 0x6A,
+    //    MapValuesBytes = 0x6B,
+    //    MapValuesInteger = 0x6C,
+    //    MapValuesFloat = 0x6D,
+    //    MapValuesMap = 0x6E,
+    //    MapValuesString = 0x6F,
+    ///////////////////////////////////////////////////////////////////////
+    // String operator codes (start at 0x70)
+    StringAsBoolean = 0x70,
+    //    StringAsBytes = 0x71,
+    StringAsFloat = 0x72,
+    StringAsInteger = 0x73,
+    StringLength = 0x74,
+    StringMatch = 0x75,
+    //    StringParseJSONArray = 0x76,
+    //    StringParseJSONBoolean = 0x77,
+    //    StringParseJSONInteger = 0x78,
+    //    StringParseJSONFloat = 0x79,
+    //    StringParseJSONMap = 0x7A,
+    //    StringParseJSONString = 0x7B,
+    //    StringParseXML = 0x7C,
+    StringToLowerCase = 0x7D,
+    StringToUpperCase = 0x7E,
+    ///////////////////////////////////////////////////////////////////////
+    //  Mixed operator codes (start at 0x80)
+    MixedAsArray = 0x80,
+    MixedAsBoolean = 0x81,
+    MixedAsFloat = 0x82,
+    MixedAsInteger = 0x83,
+    MixedAsMap = 0x84,
+    MixedAsString = 0x85,
+    //    MixedHash = 0x86,
+
+    // Old operator codes (start at 0xA0)
+    Get = 0xA0,
+    BooleanAsString = 0xA1,
+    IntegerAsMixed = 0xA2,
+    FloatAsMixed = 0xA3,
+    StringAsMixed = 0xA4,
+    StringParseJSON = 0xA5,
+    ArrayGet = 0xA6,
+    MapGet = 0xA7,
     /// Flatten a map into an Array containing only the values but not the keys
-    MapValues = 0x63,
-    // Mixed operator codes (start at 0x70)
-    MixedAsArray = 0x70,
-    MixedAsBoolean = 0x71,
-    MixedAsFloat = 0x72,
-    MixedAsInteger = 0x73,
-    MixedAsMap = 0x74,
-    MixedAsString = 0x75,
-    //    MixedHash = 0x76,
-
-    // Result operator codes (start at 0x80)
-    //    ResultGet = 0x80,
-    //    ResultGetOr = 0x81,
-    //    ResultIsOk = 0x82,
-
-    // Bytes operator codes (start at 0x90)
-    BytesAsString = 0x90,
-    BytesHash = 0x91,
+    MapValues = 0xA8,
 }
 
 impl fmt::Display for RadonOpCodes {
