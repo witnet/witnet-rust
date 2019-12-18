a6096a9d8e8493a3365739f49a76fc18354be733
Luis Rubio <luisr@stampery.co>
2019-12-18 16:10:10 +0100

    fix(rad): fix mode tests

--- rad/src/reducers/mode.rs
+++ rad/src/reducers/mode.rs
@@ -39,92 +39,90 @@ pub fn mode(input: &RadonArray) -> Result<RadonTypes, RadError> {
     }
 }
 
-#[test]
-fn test_operate_reduce_mode_float() {
-    use crate::types::float::RadonFloat;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let input = RadonArray::from(vec![
-        RadonFloat::from(1f64).into(),
-        RadonFloat::from(2f64).into(),
-        RadonFloat::from(2f64).into(),
-    ]);
-    let _expected = RadonTypes::from(RadonFloat::from(2f64));
-    let output = mode(&input).unwrap();
-    assert_eq!(output, _expected);
-}
+    use crate::{
+        error::RadError::{ModeEmpty, ModeTie},
+        types::{float::RadonFloat, integer::RadonInteger, string::RadonString},
+    };
 
-#[test]
-fn test_operate_reduce_mode_float_invalid() {
-    use crate::types::float::RadonFloat;
+    #[test]
+    fn test_operate_reduce_mode_float() {
+        let input = RadonArray::from(vec![
+            RadonFloat::from(1f64).into(),
+            RadonFloat::from(2f64).into(),
+            RadonFloat::from(2f64).into(),
+        ]);
+        let _expected = RadonTypes::from(RadonFloat::from(2f64));
+        let output = mode(&input).unwrap();
+        assert_eq!(output, _expected);
+    }
 
-    let input = RadonArray::from(vec![
-        RadonFloat::from(1f64).into(),
-        RadonFloat::from(2f64).into(),
-    ]);
+    #[test]
+    fn test_operate_reduce_mode_float_invalid() {
+        let input = RadonArray::from(vec![
+            RadonFloat::from(1f64).into(),
+            RadonFloat::from(2f64).into(),
+        ]);
 
-    let output = mode(&input).unwrap_err();
+        let output = mode(&input).unwrap_err();
+        let expected_error = ModeTie { values: input };
+        assert_eq!(output, expected_error);
+    }
 
-    assert_eq!(output.to_string(), "There was a tie after applying the mode reducer on values: `RadonArray { value: [Float(RadonFloat { value: 1.0 }), Float(RadonFloat { value: 2.0 })], inner_type: Discriminant(3) }`".to_string());
-}
+    #[test]
+    fn test_operate_reduce_mode_int() {
+        let input = RadonArray::from(vec![
+            RadonInteger::from(1i128).into(),
+            RadonInteger::from(2i128).into(),
+            RadonInteger::from(2i128).into(),
+        ]);
+        let _expected = RadonTypes::from(RadonInteger::from(2i128));
+        let output = mode(&input).unwrap();
+        assert_eq!(output, _expected);
+    }
 
-#[test]
-fn test_operate_reduce_mode_int() {
-    use crate::types::integer::RadonInteger;
+    #[test]
+    fn test_operate_reduce_mode_int_invalid() {
+        let input = RadonArray::from(vec![
+            RadonInteger::from(1i128).into(),
+            RadonInteger::from(2i128).into(),
+        ]);
+        let output = mode(&input).unwrap_err();
+        let expected_error = ModeTie { values: input };
+        assert_eq!(output, expected_error);
+    }
 
-    let input = RadonArray::from(vec![
-        RadonInteger::from(1i128).into(),
-        RadonInteger::from(2i128).into(),
-        RadonInteger::from(2i128).into(),
-    ]);
-    let _expected = RadonTypes::from(RadonInteger::from(2i128));
-    let output = mode(&input).unwrap();
-    assert_eq!(output, _expected);
-}
+    #[test]
+    fn test_operate_reduce_mode_str() {
+        let input = RadonArray::from(vec![
+            RadonString::from("Hello world!").into(),
+            RadonString::from("Hello world!").into(),
+            RadonString::from("Bye world!").into(),
+        ]);
+        let expected = RadonString::from("Hello world!").into();
+        let output = mode(&input).unwrap();
+        assert_eq!(output, expected);
+    }
 
-#[test]
-fn test_operate_reduce_mode_int_invalid() {
-    use crate::types::integer::RadonInteger;
+    #[test]
+    fn test_operate_reduce_mode_str_invalid() {
+        let input = RadonArray::from(vec![
+            RadonString::from("Hello world!").into(),
+            RadonString::from("Bye world!").into(),
+        ]);
+        let output = mode(&input).unwrap_err();
+        let expected_error = ModeTie { values: input };
+        assert_eq!(output, expected_error);
+    }
 
-    let input = RadonArray::from(vec![
-        RadonInteger::from(1i128).into(),
-        RadonInteger::from(2i128).into(),
-    ]);
-    let output = mode(&input).unwrap_err();
-    assert_eq!(output.to_string(), "There was a tie after applying the mode reducer on values: `RadonArray { value: [Integer(RadonInteger { value: 1 }), Integer(RadonInteger { value: 2 })], inner_type: Discriminant(4) }`".to_string());
-}
-
-#[test]
-fn test_operate_reduce_mode_str() {
-    use crate::types::string::RadonString;
-
-    let input = RadonArray::from(vec![
-        RadonString::from("Hello world!").into(),
-        RadonString::from("Hello world!").into(),
-        RadonString::from("Bye world!").into(),
-    ]);
-    let expected = RadonString::from("Hello world!").into();
-    let output = mode(&input).unwrap();
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn test_operate_reduce_mode_str_invalid() {
-    use crate::types::string::RadonString;
-
-    let input = RadonArray::from(vec![
-        RadonString::from("Hello world!").into(),
-        RadonString::from("Bye world!").into(),
-    ]);
-    let output = mode(&input).unwrap_err();
-    assert_eq!(output.to_string(), "There was a tie after applying the mode reducer on values: `RadonArray { value: [String(RadonString { value: \"Hello world!\" }), String(RadonString { value: \"Bye world!\" })], inner_type: Discriminant(7) }`");
-}
-
-#[test]
-fn test_operate_reduce_mode_empty() {
-    let input = RadonArray::from(vec![]);
-    let output = mode(&input).unwrap_err();
-    assert_eq!(
-        output.to_string(),
-        "Tried to apply mode reducer on an empty array"
-    );
+    #[test]
+    fn test_operate_reduce_mode_empty() {
+        let input = RadonArray::from(vec![]);
+        let output = mode(&input).unwrap_err();
+        let expected_error = ModeEmpty;
+        assert_eq!(output, expected_error);
+    }
 }
