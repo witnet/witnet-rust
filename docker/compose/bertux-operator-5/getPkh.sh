36945e41638901159cc2b6255cefa466bd088d28
Bertrand Juglas <b@juglas.name>
2020-03-13 19:42:08 +0100

    chore(docker): include @bertux example config for docker-compose

---
+++ docker/compose/bertux-operator-5/getPkh.sh
@@ -0,0 +1,5 @@
+docker-compose exec node ./witnet node getPkh
+docker-compose exec node2 ./witnet node getPkh
+docker-compose exec node3 ./witnet node getPkh
+docker-compose exec node4 ./witnet node getPkh
+docker-compose exec node5 ./witnet node getPkh
---
+++ docker/compose/bertux-operator-stable/getLastBlock.sh
@@ -0,0 +1,1 @@
+docker-compose exec node ./witnet node blockchain --epoch=-1 --limit=1
