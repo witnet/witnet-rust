f83ee96d50f725a62a0a7fba51965e2ea0afc197
Bertrand Juglas <b@juglas.name>
2020-03-16 20:13:07 +0100

    operator scripts adapted to 5 nodes

---
+++ docker/compose/operator-5/getLastBlock.sh
@@ -0,0 +1,5 @@
+docker-compose exec node ./witnet node blockchain --epoch=-1 --limit=1
+docker-compose exec node2 ./witnet node blockchain --epoch=-1 --limit=1
+docker-compose exec node3 ./witnet node blockchain --epoch=-1 --limit=1
+docker-compose exec node4 ./witnet node blockchain --epoch=-1 --limit=1
+docker-compose exec node5 ./witnet node blockchain --epoch=-1 --limit=1
---
+++ docker/compose/operator-5/getReputation.sh
@@ -0,0 +1,5 @@
+docker-compose exec node ./witnet node getReputation
+docker-compose exec node2 ./witnet node getReputation
+docker-compose exec node3 ./witnet node getReputation
+docker-compose exec node4 ./witnet node getReputation
+docker-compose exec node5 ./witnet node getReputation
