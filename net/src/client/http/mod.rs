b7505c62e64d60d507e6ed0860fa488498a5c203
Adán SDPC <adan.sdpc@gmail.com>
2020-09-08 15:58:51 +0200

    feat(net): create `WitnetHttpClient` with support for proxies

---
+++ net/src/client/http/mod.rs
@@ -0,0 +1,62 @@
+use std::str::FromStr;
+use std::sync::Arc;
+
+use futures03::future::BoxFuture;
+use surf::http::{self, Response, Uri};
+use surf::middleware::{Body, HttpClient as SurfHttpClient};
+use surf::Client;
+
+type Request = http::Request<Body>;
+
+/// A surf-alike HTTP client that additionally supports proxies (HTTP(S), SOCKS4 and SOCKS5)
+#[derive(Clone, Debug)]
+pub struct WitnetHttpClient {
+    client: Arc<isahc::HttpClient>,
+}
+
+impl WitnetHttpClient {
+    /// Create a new `WitnetHttpClient`
+    pub fn new(proxy: &Option<&str>) -> Self {
+        let proxy_address = proxy.map(|x| Uri::from_str(x).unwrap());
+        let client = Arc::new(
+            if let Some(proxy_address) = proxy_address {
+                isahc::HttpClient::builder().proxy(proxy_address)
+            } else {
+                isahc::HttpClient::builder()
+            }
+            .build()
+            .unwrap(),
+        );
+
+        Self { client }
+    }
+
+    /// Turn this `WitnetHttpClient` into a `surf`-compatible client
+    pub fn as_surf_client(&self) -> Client<Self> {
+        Client::with_client(self.clone())
+    }
+}
+
+impl SurfHttpClient for WitnetHttpClient {
+    type Error = isahc::Error;
+
+    fn send(&self, req: Request) -> BoxFuture<'static, Result<Response<Body>, Self::Error>> {
+        let client = self.client.clone();
+        Box::pin(async move {
+            // Compose HTTP request
+            let (parts, body) = req.into_parts();
+            let body = isahc::Body::reader(body);
+            let req: http::Request<isahc::Body> = http::Request::from_parts(parts, body);
+
+            // Send HTTP request and wait for response
+            let res = client.send_async(req).await?;
+
+            // Read HTTP response
+            let (parts, body) = res.into_parts();
+            let body = Body::from_reader(body);
+            let res = http::Response::from_parts(parts, body);
+
+            Ok(res)
+        })
+    }
+}
--- net/src/client/mod.rs
+++ net/src/client/mod.rs
@@ -1,3 +1,6 @@
 //! Client implementations.
 
+/// HTTP clients
+pub mod http;
+/// TCP clients
 pub mod tcp;
