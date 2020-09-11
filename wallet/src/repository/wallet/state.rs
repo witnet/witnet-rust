ff923e70f1916aed3e2f6d4ca009c8d30a5333a2
Mario Cao <mariocao@gmail.com>
2020-09-11 20:49:00 +0200

    docs(wallet): add wallet state docs

--- wallet/src/repository/wallet/state.rs
+++ wallet/src/repository/wallet/state.rs
@@ -1,26 +1,40 @@
 use super::*;
 use witnet_data_structures::chain::EpochConstants;
 
+/// A single wallet state. It includes:
+///  - fields required to operate wallet accounts (e.g. derive addresses)
+///  - on-memory state after indexing pending block transactions
 pub struct State {
-    pub name: Option<String>,
-    pub caption: Option<String>,
+    /// Current account index
     pub account: u32,
-    pub keychains: [types::ExtendedSK; 2],
-    pub next_external_index: u32,
-    pub next_internal_index: u32,
+    /// Available account indices
     pub available_accounts: Vec<u32>,
+    /// Current wallet balance (including pending movements)
     pub balance: u64,
-    pub transaction_next_id: u32,
-    pub utxo_set: model::UtxoSet,
+    /// Wallet caption
+    pub caption: Option<String>,
+    /// Epoch constants
     pub epoch_constants: EpochConstants,
-    /// Beacon of the last block received during synchronization.
-    pub last_sync: CheckpointBeacon,
-    /// Last block beacon confirmed by superblock (or during sync process)
+    /// Keychains used to derive addresses
+    pub keychains: [types::ExtendedSK; 2],
+    /// Beacon of last block confirmed by superblock (or during sync process)
     pub last_confirmed: CheckpointBeacon,
-    /// List of pending balance movements, waiting to be confirmed with a superblock
-    pub pending_movements: HashMap<String, Vec<model::BalanceMovement>>,
+    /// Beacon of the last block received during synchronization
+    pub last_sync: CheckpointBeacon,
+    /// Wallet name
+    pub name: Option<String>,
+    /// Next external index used to derive addresses
+    pub next_external_index: u32,
+    /// Next internal index used to derive addresses
+    pub next_internal_index: u32,
     /// List of pending address infos, waiting to be confirmed with a superblock
     pub pending_address_infos: HashMap<String, Vec<model::AddressInfo>>,
     /// List of pending blocks waiting to be confirmed
     pub pending_blocks: HashMap<String, model::Beacon>,
+    /// List of pending balance movements, waiting to be confirmed with a superblock
+    pub pending_movements: HashMap<String, Vec<model::BalanceMovement>>,
+    /// Next transaction identifier of the wallet
+    pub transaction_next_id: u32,
+    /// Current UTXO set (including pending movements)
+    pub utxo_set: model::UtxoSet,
 }
