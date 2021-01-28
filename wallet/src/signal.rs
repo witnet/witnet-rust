53a7bc04985d6d27da619946ffa7f757ca1a678d
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-01-28 16:48:23 +0100

    fix(wallet): update tokio::signal used in windows

--- wallet/src/signal.rs
+++ wallet/src/signal.rs
@@ -1,19 +1,16 @@
 //! # Signal handling utility functions
-use futures01::{Future, Stream};
-use futures_util::compat::Compat01As03;
 
 /// It will call `cb` function for Ctrl-c events (or SIGTERM signals in Unix).
+#[cfg(unix)]
 pub fn ctrl_c<T: Fn() + 'static>(cb: T) {
+    use futures01::{Future, Stream};
+    use futures_util::compat::Compat01As03;
+
     // This is received when doing kill $(pidof witnet), removing this handler will make the wallet
     // shutdown instantly and corrupt the database.
-    #[cfg(unix)]
     let sigterm = tokio_signal::unix::Signal::new(tokio_signal::unix::SIGTERM)
         .map(|s| s.map(|_| log::trace!("Received SIGTERM signal")))
         .flatten_stream();
-
-    // There is no equivalent to SIGTERM on Windows, so use empty stream
-    #[cfg(windows)]
-    let sigterm = futures01::stream::empty();
 
     // This is received when pressing CTRL-C, and it works on both Unix and Windows
     let ctrl_c = tokio_signal::ctrl_c()
@@ -30,6 +27,22 @@ pub fn ctrl_c<T: Fn() + 'static>(cb: T) {
         .map_err(|_| ());
 
     let f = futures::FutureExt::map(Compat01As03::new(handle_shutdown), |_res| ());
+
+    actix::spawn(f);
+}
+
+/// It will call `cb` function for Ctrl-c events (or SIGTERM signals in Unix).
+#[cfg(windows)]
+pub fn ctrl_c<T: Fn() + 'static>(cb: T) {
+    let f = async move {
+        // This is received when pressing CTRL-C, and it works on both Unix and Windows
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to listen for CTRL-C event");
+        log::trace!("Received CTRL-C");
+
+        cb();
+    };
 
     actix::spawn(f);
 }
