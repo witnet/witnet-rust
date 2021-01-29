e50ad0b3c8b16e4421478e45e51be92ac895ffc9
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-01-29 12:57:05 +0100

    feat(wallet): update tokio_signal ctrl-c handler

--- wallet/src/signal.rs
+++ wallet/src/signal.rs
@@ -1,47 +1,39 @@
 //! # Signal handling utility functions
 
 /// It will call `cb` function for Ctrl-c events (or SIGTERM signals in Unix).
-#[cfg(unix)]
-pub fn ctrl_c<T: Fn() + 'static>(cb: T) {
-    use futures01::{Future, Stream};
-    use futures_util::compat::Compat01As03;
+pub fn ctrl_c<T: Fn() + 'static + Clone>(cb: T) {
+    #[cfg(unix)]
+    register_sigterm_handler(cb.clone());
 
-    // This is received when doing kill $(pidof witnet), removing this handler will make the wallet
-    // shutdown instantly and corrupt the database.
-    let sigterm = tokio_signal::unix::Signal::new(tokio_signal::unix::SIGTERM)
-        .map(|s| s.map(|_| log::trace!("Received SIGTERM signal")))
-        .flatten_stream();
+    let f = async move {
+        loop {
+            // This is received when pressing CTRL-C, and it works on both Unix and Windows
+            tokio::signal::ctrl_c()
+                .await
+                .expect("failed to listen for CTRL-C event");
+            log::trace!("Received CTRL-C");
 
-    // This is received when pressing CTRL-C, and it works on both Unix and Windows
-    let ctrl_c = tokio_signal::ctrl_c()
-        .flatten_stream()
-        .inspect(|_| log::trace!("Received CTRL-C"));
-
-    // Handle both CTRL-C and SIGTERM using the same callback
-    let handle_shutdown = ctrl_c
-        .select(sigterm)
-        .for_each(move |_| {
             cb();
-            Ok(())
-        })
-        .map_err(|_| ());
-
-    let f = futures::FutureExt::map(Compat01As03::new(handle_shutdown), |_res| ());
+        }
+    };
 
     actix::spawn(f);
 }
 
-/// It will call `cb` function for Ctrl-c events (or SIGTERM signals in Unix).
-#[cfg(windows)]
-pub fn ctrl_c<T: Fn() + 'static>(cb: T) {
+#[cfg(unix)]
+fn register_sigterm_handler<T: Fn() + 'static>(cb: T) {
     let f = async move {
-        // This is received when pressing CTRL-C, and it works on both Unix and Windows
-        tokio::signal::ctrl_c()
-            .await
-            .expect("failed to listen for CTRL-C event");
-        log::trace!("Received CTRL-C");
+        loop {
+            // This is received when doing kill $(pidof witnet), removing this handler will make the wallet
+            // shutdown instantly and corrupt the database.
+            let mut sigterm_stream =
+                tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+                    .expect("failed to register SIGTERM handler");
+            sigterm_stream.recv().await;
+            log::trace!("Received SIGTERM signal");
 
-        cb();
+            cb();
+        }
     };
 
     actix::spawn(f);
