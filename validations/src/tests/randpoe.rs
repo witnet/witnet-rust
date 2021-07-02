17e3f82a939a4ec348350f6ed5493d307b86a682
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-07-02 16:27:18 +0200

    refactor: use assert_abs_diff_eq to compare floats in tests

--- validations/src/tests/randpoe.rs
+++ validations/src/tests/randpoe.rs
@@ -236,149 +236,88 @@ fn calculate_mining_probs(v_rep: Vec<u32>, rf: u32, bf: u32) -> (Vec<f64>, f64) 
     (probs, new_prob)
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf1_bf1() {
     let v_rep = vec![10, 8, 8, 8, 5, 5, 5, 5, 0, 0];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 1, 1);
 
     for &prob in &probs[0..8] {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (7.12_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.0712, epsilon = 1e-4);
     }
 
     for &prob in &probs[8..10] {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (4.09_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.0409, epsilon = 1e-4);
     }
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (3.89_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0389, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf1_bf2() {
     let v_rep = vec![10, 8, 8, 8, 5, 5, 5, 5, 2, 2];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 1, 2);
 
     for prob in probs {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (9.04_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.0904, epsilon = 1e-4);
     }
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (4.56_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0456, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf2_bf2() {
     let v_rep = vec![10, 8, 8, 8, 5, 5, 5, 5, 2, 2];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 2, 2);
 
     for prob in probs {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (8.93_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.0893, epsilon = 1e-4);
     }
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (2.15_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0215, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf4_bf8() {
     let v_rep = vec![10, 8, 8, 8, 5, 5, 5, 5, 2, 2];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 4, 8);
 
     for prob in probs {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (10.02_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.1002, epsilon = 1e-4);
     }
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (0.25_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0025, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf1_bf1_10() {
     let v_rep = vec![10; 10];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 1, 1);
 
-    assert_eq!(
-        (probs[0] * 10_000.0).round() as u32,
-        (6.51_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(probs[0], 0.0651, epsilon = 1e-4);
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (3.49_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0349, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf1_bf1_100() {
     let v_rep = vec![10; 100];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 1, 1);
 
-    assert_eq!(
-        (probs[0] * 10_000.0).round() as u32,
-        (0.63_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(probs[0], 0.0063, epsilon = 1e-4);
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (0.37_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0037, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf4_bf8_100() {
     let v_rep = vec![10; 100];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 4, 8);
 
-    assert_eq!(
-        (probs[0] * 10_000.0).round() as u32,
-        (1.0_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(probs[0], 0.01, epsilon = 1e-4);
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (0.08_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0008, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf4_bf8_100_diff() {
     let mut v_rep = vec![10; 25];
@@ -387,45 +326,22 @@ fn calculate_mining_probabilities_rf4_bf8_100_diff() {
     v_rep.extend(vec![4; 25]);
     let (probs, new_prob) = calculate_mining_probs(v_rep, 4, 8);
 
-    assert_eq!(
-        (probs[0] * 10_000.0).round() as u32,
-        (1_f64 * 100.0).round() as u32
-    );
-    assert_eq!(
-        (probs[25] * 10_000.0).round() as u32,
-        (1_f64 * 100.0).round() as u32
-    );
-    assert_eq!(
-        (probs[50] * 10_000.0).round() as u32,
-        (1_f64 * 100.0).round() as u32
-    );
-    assert_eq!(
-        (probs[75] * 10_000.0).round() as u32,
-        (1_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(probs[0], 0.01, epsilon = 1e-4);
+    assert_abs_diff_eq!(probs[25], 0.01, epsilon = 1e-4);
+    assert_abs_diff_eq!(probs[50], 0.01, epsilon = 1e-4);
+    assert_abs_diff_eq!(probs[75], 0.01, epsilon = 1e-4);
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (0.08_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0008, epsilon = 1e-4);
 }
 
-// FIXME: Allow for now, wait for https://github.com/rust-lang/rust/issues/67058 to reach stable
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 #[test]
 fn calculate_mining_probabilities_rf_high() {
     let v_rep = vec![10, 8, 8, 2];
     let (probs, new_prob) = calculate_mining_probs(v_rep, 4, 8);
 
     for prob in probs {
-        assert_eq!(
-            (prob * 10_000.0).round() as u32,
-            (25_f64 * 100.0).round() as u32
-        );
+        assert_abs_diff_eq!(prob, 0.25, epsilon = 1e-4);
     }
 
-    assert_eq!(
-        (new_prob * 10_000.0).round() as u32,
-        (0_f64 * 100.0).round() as u32
-    );
+    assert_abs_diff_eq!(new_prob, 0.0, epsilon = 1e-4);
 }
