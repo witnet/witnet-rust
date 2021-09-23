6dd361096105257aab3ded70667c5024bb8ed6b9
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-09-23 17:12:43 +0200

    chore: update aes crate

--- crypto/src/cipher.rs
+++ crypto/src/cipher.rs
@@ -22,7 +22,7 @@ pub enum Error {
 
 /// Encrypt data with AES CBC using the supplied secret
 pub fn encrypt_aes_cbc(secret: &[u8], plaintext: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
-    let cipher = Aes256Cbc::new_var(secret, iv).map_err(Error::InvalidKeyIvLength)?;
+    let cipher = Aes256Cbc::new_from_slices(secret, iv).map_err(Error::InvalidKeyIvLength)?;
     let ciphertext = cipher.encrypt_vec(plaintext);
 
     Ok(ciphertext)
@@ -30,7 +30,7 @@ pub fn encrypt_aes_cbc(secret: &[u8], plaintext: &[u8], iv: &[u8]) -> Result<Vec
 
 /// Decrypt data with AES CBC using the supplied secret
 pub fn decrypt_aes_cbc(secret: &[u8], ciphertext: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
-    let cipher = Aes256Cbc::new_var(secret, iv).map_err(Error::InvalidKeyIvLength)?;
+    let cipher = Aes256Cbc::new_from_slices(secret, iv).map_err(Error::InvalidKeyIvLength)?;
     let plaintext = cipher
         .decrypt_vec(ciphertext)
         .map_err(Error::BlockModeError)?;
