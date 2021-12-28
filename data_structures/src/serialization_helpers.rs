7bb42fee674fbac2aa09898b28706570ba5cb1bd
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2021-12-28 18:13:34 +0100

    feat: make RADRetrieve backwards compatible when using bincode
    The problem is that while adding new fields is fine when using protobuf,
    when using bincode that is not allowed. Also the serde attribute
    "skip_serialization_if" is not supported by bincode.

--- data_structures/src/serialization_helpers.rs
+++ data_structures/src/serialization_helpers.rs
@@ -7,12 +7,15 @@
 // Ideally all this code would be generated with a `#[serde(human_readable_string)]` macro.
 
 use crate::{
-    chain::{GenesisBlockInfo, Hash, OutputPointer, PublicKeyHash, ValueTransferOutput, SHA256},
+    chain::{
+        GenesisBlockInfo, Hash, OutputPointer, PublicKeyHash, RADRetrieve, RADType,
+        ValueTransferOutput, SHA256,
+    },
     get_environment,
     utxo_pool::UtxoSelectionStrategy,
 };
 use serde::{
-    de::{self, IntoDeserializer, MapAccess, Visitor},
+    de::{self, IntoDeserializer, MapAccess, SeqAccess, Visitor},
     Deserialize, Deserializer, Serialize, Serializer,
 };
 use std::{fmt, fmt::Display, str::FromStr};
@@ -329,5 +332,282 @@ impl<'de> Deserialize<'de> for UtxoSelectionStrategy {
         D: Deserializer<'de>,
     {
         deserializer.deserialize_any(UtxoSelectionStrategyVisitor)
+    }
+}
+
+/// Serialization helper for `RADRetrieve`.
+/// Serializes `RADRetrieve` as a 2-field struct: `(db_version: u32, rad_retrieve: RADRetrieve)`.
+/// Deserializes `RADRetrieve` in a backwards compatible way with bincode.
+#[derive(Serialize)]
+struct RADRetrieveSerializationHelperVersioned(u32, RADRetrieveSerializationHelperBincode);
+
+impl RADRetrieveSerializationHelperVersioned {
+    const LATEST_VERSION: u32 = 3;
+}
+
+/// This should be the same as `RADRetrieve`, it exists because we want to use the automatically
+/// derived serialization code.
+#[derive(Serialize, Deserialize)]
+struct RADRetrieveSerializationHelperJson {
+    /// Kind of retrieval
+    pub kind: RADType,
+    /// URL
+    #[serde(default, skip_serializing_if = "String::is_empty")]
+    pub url: String,
+    /// Serialized RADON script
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub script: Vec<u8>,
+    /// Body of a HTTP-POST request
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub body: Vec<u8>,
+    /// Extra headers of a HTTP-GET or HTTP-POST request
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub headers: Vec<(String, String)>,
+}
+
+/// This should be the same as `RADRetrieveSerializationHelperJson`, but bincode does not support
+/// the serde attribute `skip_serializing_if`.
+#[derive(Serialize, Deserialize)]
+struct RADRetrieveSerializationHelperBincode {
+    /// Kind of retrieval
+    pub kind: RADType,
+    /// URL
+    pub url: String,
+    /// Serialized RADON script
+    pub script: Vec<u8>,
+    /// Body of a HTTP-POST request
+    pub body: Vec<u8>,
+    /// Extra headers of a HTTP-GET or HTTP-POST request
+    pub headers: Vec<(String, String)>,
+}
+
+impl From<RADRetrieve> for RADRetrieveSerializationHelperVersioned {
+    fn from(x: RADRetrieve) -> Self {
+        let db_version = Self::LATEST_VERSION;
+
+        let RADRetrieve {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        } = x;
+
+        Self(
+            db_version,
+            RADRetrieveSerializationHelperBincode {
+                kind,
+                url,
+                script,
+                body,
+                headers,
+            },
+        )
+    }
+}
+
+impl From<RADRetrieveSerializationHelperVersioned> for RADRetrieve {
+    fn from(x: RADRetrieveSerializationHelperVersioned) -> Self {
+        let RADRetrieveSerializationHelperVersioned(db_version, rad_retrieve) = x;
+
+        assert_eq!(
+            db_version,
+            RADRetrieveSerializationHelperVersioned::LATEST_VERSION
+        );
+
+        let RADRetrieveSerializationHelperBincode {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        } = rad_retrieve;
+
+        Self {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        }
+    }
+}
+
+impl From<RADRetrieveSerializationHelperBincode> for RADRetrieveSerializationHelperVersioned {
+    fn from(rad_retrieve: RADRetrieveSerializationHelperBincode) -> Self {
+        let db_version = Self::LATEST_VERSION;
+
+        Self(db_version, rad_retrieve)
+    }
+}
+
+impl From<RADRetrieve> for RADRetrieveSerializationHelperJson {
+    fn from(x: RADRetrieve) -> Self {
+        let RADRetrieve {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        } = x;
+
+        Self {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        }
+    }
+}
+
+impl From<RADRetrieveSerializationHelperJson> for RADRetrieve {
+    fn from(x: RADRetrieveSerializationHelperJson) -> Self {
+        let RADRetrieveSerializationHelperJson {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        } = x;
+
+        Self {
+            kind,
+            url,
+            script,
+            body,
+            headers,
+        }
+    }
+}
+
+struct RADRetrieveSerializationHelperVersionedVisitor;
+
+impl<'de> Visitor<'de> for RADRetrieveSerializationHelperVersionedVisitor {
+    type Value = RADRetrieveSerializationHelperVersioned;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "struct RADRetrieveSerializationHelperVersioned")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        // Always deserialize as the latest version
+        let latest_version = Self::Value::LATEST_VERSION;
+
+        // Ensure backwards compatibility when using bincode.
+        // The first element of the old version of RADRetrieve was a RADType, which is a simple enum
+        // with 3 fields. This enum is serialized by bincode as a u32, with the possible values 0,
+        // 1, 2.
+        // The new helper type RADRetrieveSerializationHelperVersioned is serialized with a u32
+        // db_version field as the first field.
+        // Therefore, we can deserialize the first u32 value, and select the correct strategy
+        // depending on it. If the db_version is 0, 1, or 2, this is the old version RADRetrieve so
+        // we need to deserialize the two missing fields (url, script) next. Otherwise, this is the
+        // actual db_version value, so we can use it to select the correct helper.
+        // Currently there is only one helper: RADRetrieveSerializationHelperBincode, which uses
+        // db_version 3.
+        let db_version: u32 = seq
+            .next_element()?
+            .ok_or_else(|| de::Error::missing_field("db_version"))?;
+
+        match db_version {
+            0 | 1 | 2 => {
+                let kind = match db_version {
+                    0 => RADType::Unknown,
+                    1 => RADType::HttpGet,
+                    2 => RADType::Rng,
+                    _ => unreachable!(),
+                };
+                // In the original definition, `RADRetrieve` was a 3-field struct, and in this
+                // implementation it is a 2-field struct. Since we have already deserialized the
+                // value of the first field, we treat the 2 remaining fields `url` and `script` as
+                // if it was one field with value `(url, script)`.
+                // Treating them as 2 separate fields does not work because `seq.next_element()`
+                // returns `None` for the second element, because
+                // `RADRetrieveSerializationHelperVersioned` does only have 2 fields.
+                // This could cause problems in some serialization formats where `(a, b, c)` is not
+                // equivalent to `(a, (b, c))`, but bincode seems to be happy, and we only care
+                // about being backwards compatible with bincode.
+                let (url, script): (String, Vec<u8>) = seq
+                    .next_element()?
+                    .ok_or_else(|| de::Error::missing_field("rad_retrieve"))?;
+
+                // The new fields `body` and `headers` which were missing in this version of
+                // `RADRetrieve` will have the default value
+                let rad_retrieve = RADRetrieveSerializationHelperBincode {
+                    kind,
+                    url,
+                    script,
+                    body: vec![],
+                    headers: vec![],
+                };
+
+                Ok(RADRetrieveSerializationHelperVersioned(
+                    latest_version,
+                    rad_retrieve,
+                ))
+            }
+            3 => {
+                // Version 3: deserialize as 2-field struct: `(db_version, rad_retrieve)`.
+                let rad_retrieve: RADRetrieveSerializationHelperBincode = seq
+                    .next_element()?
+                    .ok_or_else(|| de::Error::missing_field("rad_retrieve"))?;
+
+                Ok(RADRetrieveSerializationHelperVersioned(
+                    latest_version,
+                    rad_retrieve,
+                ))
+            }
+            unknown_version => Err(de::Error::custom(format!(
+                "RADRetrieve: unknown db_version {}, expected one of 0, 1, 2, 3",
+                unknown_version
+            ))),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for RADRetrieveSerializationHelperVersioned {
+    fn deserialize<D>(deserializer: D) -> Result<RADRetrieveSerializationHelperVersioned, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // Deserialize using visitor. This is a 2-field tuple struct:
+        // (db_version: u32, rad_retrieve: RADRetrieve)
+        deserializer.deserialize_tuple_struct(
+            "RADRetrieveSerializationHelperVersioned",
+            2,
+            RADRetrieveSerializationHelperVersionedVisitor,
+        )
+    }
+}
+
+impl Serialize for RADRetrieve {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        if serializer.is_human_readable() {
+            // Serialize skipping default fields to improve readability
+            RADRetrieveSerializationHelperJson::from(self.clone()).serialize(serializer)
+        } else {
+            // Serialize by prepending a `db_version: u32` field, to ensure easier migrations in the
+            // future. This always serializes in the format of the latest version.
+            RADRetrieveSerializationHelperVersioned::from(self.clone()).serialize(serializer)
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for RADRetrieve {
+    fn deserialize<D>(deserializer: D) -> Result<RADRetrieve, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        if deserializer.is_human_readable() {
+            // Deserialize by assuming that missing fields have default value
+            RADRetrieveSerializationHelperJson::deserialize(deserializer).map(Into::into)
+        } else {
+            // Deserialize by reading the format version. Ensures backwards compatibility.
+            RADRetrieveSerializationHelperVersioned::deserialize(deserializer).map(Into::into)
+        }
     }
 }
