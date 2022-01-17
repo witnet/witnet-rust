0710a50d8db45b510cb2aa20aef104ab3a38fc9e
Luis Rubio <l.rubiorod@gmail.com>
2022-01-17 13:27:26 +0100

    feat(tapi): update wip_0020_0021 signaling date

--- data_structures/src/mainnet_validations.rs
+++ data_structures/src/mainnet_validations.rs
@@ -147,9 +147,9 @@ impl TapiEngine {
                     votes: 0,
                     period: 26880,
                     wip: "WIP0020-0021".to_string(),
-                    // Start voting at
-                    // TODO: insert date here
-                    init: 999999999,
+                    // Start signaling on
+                    // 5 April 2022 @ 9:00:00 UTC
+                    init: 1032960,
                     end: u32::MAX,
                     bit,
                 };
@@ -722,10 +722,9 @@ mod tests {
         let mut t = TapiEngine::default();
 
         let (epoch, old_wips) = t.initialize_wip_information(Environment::Mainnet);
-        // TODO: Use right WIP number and properties
         // The first block whose vote must be counted is the one from WIP0021
-        let init_epoch_wip0021 = 999999999;
-        assert_eq!(epoch, init_epoch_wip0021);
+        let init_epoch_wip002021 = 1032960;
+        assert_eq!(epoch, init_epoch_wip002021);
         // The TapiEngine was just created, there list of old_wips must be empty
         assert_eq!(old_wips, HashSet::new());
         // The list of active WIPs should match those defined in `wip_info`
@@ -735,13 +734,12 @@ mod tests {
         }
         assert_eq!(t.wip_activation, hm);
 
-        // TODO: Use right WIP number and properties
         // Test initialize_wip_information with a non-empty TapiEngine
         let (epoch, old_wips) = t.initialize_wip_information(Environment::Mainnet);
         // WIP0021 is already included and it won't be updated
-        let name_wip0021 = "WIP0020-0021".to_string();
+        let name_wip002021 = "WIP0020-0021".to_string();
         let mut hs = HashSet::new();
-        hs.insert(name_wip0021);
+        hs.insert(name_wip002021);
         assert_eq!(old_wips, hs);
 
         // There is no new WIPs to update so we obtain the max value
