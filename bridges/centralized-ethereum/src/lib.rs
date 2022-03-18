0f46bd5579a9675d1a7cf0a64e4de89a2a236191
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2022-03-18 17:15:09 +0100

    fix(bridge): avoid problems with overloaded functions
    By ensuring that only one function exists with any given name

--- bridges/centralized-ethereum/src/lib.rs
+++ bridges/centralized-ethereum/src/lib.rs
@@ -29,10 +29,38 @@ pub fn create_wrb_contract(eth_client_url: &str, wrb_contract_addr: H160) -> Con
     let web3 = web3::Web3::new(web3_http);
     // Why read files at runtime when you can read files at compile time
     let wrb_contract_abi_json: &[u8] = include_bytes!("../wrb_abi.json");
-    let wrb_contract_abi = web3::ethabi::Contract::load(wrb_contract_abi_json)
+    let mut wrb_contract_abi = web3::ethabi::Contract::load(wrb_contract_abi_json)
         .map_err(|e| format!("Unable to load WRB contract from ABI: {:?}", e))
         .unwrap();
+
+    // Fix issue #2046, manually select the desired function when multiple candidates have the same name
+    // https://github.com/witnet/witnet-rust/issues/2046
+    hack_fix_functions_with_multiple_definitions(&mut wrb_contract_abi);
+
     Contract::new(web3.eth(), wrb_contract_addr, wrb_contract_abi)
+}
+
+// The web3 library does not properly support overloaded functions yet, so here we ensure that there
+// is no ambiguity and every function has only one possible definition
+fn hack_fix_functions_with_multiple_definitions(wrb_contract_abi: &mut web3::ethabi::Contract) {
+    let functions = wrb_contract_abi
+        .functions
+        .get_mut("reportResult")
+        .expect("no reportResult function in ABI");
+    // There are two candidate "reportResult" functions, we want to keep the one with 4 inputs
+    assert_eq!(functions.len(), 2);
+    functions.retain(|f| f.inputs.len() == 4);
+    assert_eq!(functions.len(), 1);
+
+    // Ensure that all functions only have one possible definition
+    for (function_name, definitions) in &wrb_contract_abi.functions {
+        assert_eq!(
+            definitions.len(),
+            1,
+            "function {:?} is duplicated in ABI",
+            function_name
+        );
+    }
 }
 
 /// Check if the witnet node is running
@@ -134,5 +162,21 @@ pub async fn handle_receipt(receipt: &TransactionReceipt) -> Result<(), ()> {
             log::error!("Unknown return code, should be 0 or 1, is: {:?}", x);
             Err(())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hack_fix_functions_with_multiple_definitions() {
+        // The hack_fix_functions_with_multiple_definitions function already does some checks
+        // internally, so here we call it to ensure the ABI is correct.
+        let wrb_contract_abi_json: &[u8] = include_bytes!("../wrb_abi.json");
+        let mut wrb_contract_abi = web3::ethabi::Contract::load(wrb_contract_abi_json)
+            .map_err(|e| format!("Unable to load WRB contract from ABI: {:?}", e))
+            .unwrap();
+        hack_fix_functions_with_multiple_definitions(&mut wrb_contract_abi);
     }
 }
