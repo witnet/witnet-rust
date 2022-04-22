2beccc96a3f9ec9ef927fda8d38f82c3578f28cd
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2022-04-22 16:28:23 +0200

    chore: update version to 1.5.2

--- data_structures/src/builders.rs
+++ data_structures/src/builders.rs
@@ -170,7 +170,7 @@ fn random_nonce() -> u64 {
 /// Function that returns the dynamic user agent
 pub fn user_agent() -> String {
     // TODO: Read version, arch and OS
-    let release = "1.5.1";
+    let release = "1.5.2";
 
     format!("witnet-rust {}", release)
 }
