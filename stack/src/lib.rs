b62432162c6a0514236105078b6ef3af98925df7
Luis Rubio <l.rubiorod@gmail.com>
2022-05-24 12:22:59 +0200

    feat(stack): Implement script execution methods with tests
    Co-authored-by: Tomasz Polaczyk <tmpolaczyk@gmail.com>

--- stack/src/lib.rs
+++ stack/src/lib.rs
@@ -113,7 +113,7 @@ pub fn encode(a: Script<MyOperator, MyValue>) -> Vec<u8> {
     serde_json::to_vec(&x).unwrap()
 }
 
-pub fn execute_script(script: Script<MyOperator, MyValue>) -> bool {
+fn execute_script(script: Script<MyOperator, MyValue>) -> bool {
     // Instantiate the machine with a reference to your operator system.
     let mut machine = Machine::new(&my_operator_system);
     let result = machine.run_script(&script);
@@ -121,10 +121,53 @@ pub fn execute_script(script: Script<MyOperator, MyValue>) -> bool {
     result == Some(&MyValue::Boolean(true))
 }
 
+fn execute_locking_script(redeem_bytes: &[u8], locking_bytes: &[u8; 20]) -> bool {
+    // Check locking script
+    let mut locking_script = vec![
+        Item::Operator(MyOperator::Hash160),
+        Item::Value(MyValue::Bytes(locking_bytes.to_vec())),
+        Item::Operator(MyOperator::Equal),
+    ];
+
+    // Push redeem script as argument
+    locking_script.insert(0, Item::Value(MyValue::Bytes(redeem_bytes.to_vec())));
+
+    // Execute the script
+    execute_script(locking_script)
+}
+
+fn execute_redeem_script(witness_bytes: &[u8], redeem_bytes: &[u8]) -> bool {
+    // Execute witness script concatenated with redeem script
+    let mut witness_script = decode(witness_bytes);
+    let redeem_script = decode(redeem_bytes);
+    witness_script.extend(redeem_script);
+
+    // Execute the script
+    execute_script(witness_script)
+}
+
+pub fn execute_complete_script(
+    witness_bytes: &[u8],
+    redeem_bytes: &[u8],
+    locking_bytes: &[u8; 20],
+) -> bool {
+    // Execute locking script
+    let result = execute_locking_script(redeem_bytes, locking_bytes);
+    if !result {
+        return false;
+    }
+
+    // Execute witness script concatenated with redeem script
+    execute_redeem_script(witness_bytes, redeem_bytes)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::execute_script;
+    const EQUAL_OPERATOR_HASH: [u8; 20] = [
+        52, 128, 191, 80, 253, 28, 169, 253, 237, 29, 0, 51, 201, 0, 31, 203, 157, 99, 218, 210,
+    ];
 
     #[test]
     fn test_execute_script() {
@@ -141,5 +184,84 @@ mod tests {
             Item::Operator(MyOperator::Equal),
         ];
         assert!(!execute_script(s));
+    }
+
+    #[test]
+    fn test_execute_locking_script() {
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        let locking_script = EQUAL_OPERATOR_HASH;
+        assert!(execute_locking_script(
+            &encode(redeem_script),
+            &locking_script
+        ));
+
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        let locking_script = [1; 20];
+        assert!(!execute_locking_script(
+            &encode(redeem_script),
+            &locking_script
+        ));
+    }
+
+    #[test]
+    fn test_execute_redeem_script() {
+        let witness = vec![
+            Item::Value(MyValue::String("patata".to_string())),
+            Item::Value(MyValue::String("patata".to_string())),
+        ];
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        assert!(execute_redeem_script(
+            &encode(witness),
+            &encode(redeem_script)
+        ));
+
+        let witness = vec![
+            Item::Value(MyValue::String("patata".to_string())),
+            Item::Value(MyValue::String("potato".to_string())),
+        ];
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        assert!(!execute_redeem_script(
+            &encode(witness),
+            &encode(redeem_script)
+        ));
+    }
+
+    #[test]
+    fn test_complete_script() {
+        let witness = vec![
+            Item::Value(MyValue::String("patata".to_string())),
+            Item::Value(MyValue::String("patata".to_string())),
+        ];
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        let locking_script = EQUAL_OPERATOR_HASH;
+        assert!(execute_complete_script(
+            &encode(witness),
+            &encode(redeem_script),
+            &locking_script,
+        ));
+
+        let witness = vec![
+            Item::Value(MyValue::String("patata".to_string())),
+            Item::Value(MyValue::String("potato".to_string())),
+        ];
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        let locking_script = EQUAL_OPERATOR_HASH;
+        assert!(!execute_complete_script(
+            &encode(witness),
+            &encode(redeem_script),
+            &locking_script,
+        ));
+
+        let witness = vec![
+            Item::Value(MyValue::String("patata".to_string())),
+            Item::Value(MyValue::String("patata".to_string())),
+        ];
+        let redeem_script = vec![Item::Operator(MyOperator::Equal)];
+        let locking_script: [u8; 20] = [1; 20];
+        assert!(!execute_complete_script(
+            &encode(witness),
+            &encode(redeem_script),
+            &locking_script,
+        ));
     }
 }
