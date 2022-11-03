8d07e949e15582712812c4cb5652a041c8745ed2
Adán SDPC <adan.sdpc@gmail.com>
2022-11-03 19:20:49 +0100

    chore: bump crate versions to 1.5.4

--- data_structures/src/builders.rs
+++ data_structures/src/builders.rs
@@ -170,7 +170,7 @@ fn random_nonce() -> u64 {
 /// Function that returns the dynamic user agent
 pub fn user_agent() -> String {
     // TODO: Read version, arch and OS
-    let release = "1.5.3";
+    let release = "1.5.4";
 
     format!("witnet-rust {}", release)
 }
