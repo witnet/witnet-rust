1872af09f3e2e8f1cd9788080ed4607aa51402c1
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-02-03 17:12:27 +0100

    chore: bump crate versions to 1.5.6

--- data_structures/src/builders.rs
+++ data_structures/src/builders.rs
@@ -170,7 +170,7 @@ fn random_nonce() -> u64 {
 /// Function that returns the dynamic user agent
 pub fn user_agent() -> String {
     // TODO: Read version, arch and OS
-    let release = "1.5.5";
+    let release = "1.5.6";
 
     format!("witnet-rust {}", release)
 }
