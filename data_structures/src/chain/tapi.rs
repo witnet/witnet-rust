b92f5cef527ba96f1acaabf9694656aac95fbb94
Adán SDPC <adan@witnet.foundation>
2023-12-31 14:21:22 +0100

    f comm

--- data_structures/src/chain/tapi.rs
+++ data_structures/src/chain/tapi.rs
@@ -37,12 +37,13 @@ const THIRD_EMERGENCY_COMMITTEE: [&str; 7] = [
 
 /// Committee for superblock indices from 224_244 until activation of 2.0
 /// (approx. from Dec 26 2023 @ 07:30pm UTC onwards)
-const FOURTH_EMERGENCY_COMMITTEE: [&str; 5] = [
+const FOURTH_EMERGENCY_COMMITTEE: [&str; 6] = [
     "wit1c0ucz2u9hwgqflkn4xjeyxtla9tqp0sderzkjr",
     "wit1drcpu0xc2akfcqn8r69vw70pj8fzjhjypdcfsq",
     "wit1drcpu3x42y5vp7w3pe203xrwpnth2pnt6c0dm9",
     "wit1llz4vw9mszjqud5pqavf84208entwz73lxekzm",
     "wit1nawuk8f2hckz6ct9wlzg95fhg3kyjzlrrd99qz",
+    "wit1sp8xnclrhm4j5lrzxwnxerdetzsjqw88kwr05f",
 ];
 
 /// 22 January 2021 @ 09:00:00 UTC
