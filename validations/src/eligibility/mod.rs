756e58239b6b5515ffae0df8e31fb4dbaed110c0
Adán SDPC <adan@witnet.foundation>
2024-02-27 16:27:26 +0100

    feat(validations): create eligibility module and isolate legacy mining logic

---
+++ validations/src/eligibility/mod.rs
@@ -0,0 +1,6 @@
+/// Eligibility-related functions specific to former versions of the Witnet protocol.
+pub mod legacy;
+
+/// Eligibility-related functions specific to the latest Proof-of-Stake version of the Witnet
+/// protocol.
+pub mod pos;
