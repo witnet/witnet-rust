f39a257ab5569fdb3b6fc21b5df5a78c568358b0
Guillermo Díaz <guillermo@otherplane.com>
2024-03-01 17:06:56 +0100

    feat(c-bridge): eth_poller: new feats
    - adapt to wsb v2.0
    - limit max batch sizes by config

--- bridges/centralized-ethereum/src/actors/eth_poller.rs
+++ bridges/centralized-ethereum/src/actors/eth_poller.rs
@@ -1,6 +1,6 @@
 use crate::{
     actors::dr_database::{
-        DrDatabase, DrInfoBridge, GetLastDrId, SetDrInfoBridge, WitnetQueryStatus,
+        DrDatabase, DrInfoBridge, DrState, GetLastDrId, SetDrInfoBridge, SetDrState, WitnetQueryStatus
     },
     config::Config,
 };
@@ -8,9 +8,10 @@ use actix::prelude::*;
 use std::{convert::TryFrom, sync::Arc, time::Duration};
 use web3::{
     contract::{self, Contract},
-    ethabi::Bytes,
+    ethabi::{Bytes, Token},
     transports::Http,
     types::U256,
+    Web3,
 };
 use witnet_node::utils::stop_system_if_panicking;
 
@@ -18,12 +19,16 @@ use witnet_node::utils::stop_system_if_panicking;
 /// in the DrDatabase
 #[derive(Default)]
 pub struct EthPoller {
+    /// Web3 object
+    pub web3: Option<Web3<web3::transports::Http>>,
     /// WRB contract
     pub wrb_contract: Option<Arc<Contract<web3::transports::Http>>>,
     /// Period to check for new requests in the WRB
-    pub eth_new_dr_polling_rate_ms: u64,
+    pub polling_rate_ms: u64,
     /// Skip first requests up to index n when updating database
     pub skip_first: u64,
+    /// Max number of queries to be batched together
+    pub max_batch_size: u16,
 }
 
 impl Drop for EthPoller {
@@ -44,7 +49,7 @@ impl Actor for EthPoller {
 
         self.check_new_requests_from_ethereum(
             ctx,
-            Duration::from_millis(self.eth_new_dr_polling_rate_ms),
+            Duration::from_millis(self.polling_rate_ms),
         );
     }
 }
@@ -57,22 +62,28 @@ impl SystemService for EthPoller {}
 
 impl EthPoller {
     /// Initialize `PeersManager` taking the configuration from a `Config` structure
-    pub fn from_config(config: &Config, wrb_contract: Arc<Contract<Http>>) -> Self {
+    pub fn from_config(config: &Config, web3: Web3<Http>, wrb_contract: Arc<Contract<Http>>) -> Self {
         Self {
+            web3: Some(web3),
             wrb_contract: Some(wrb_contract),
-            eth_new_dr_polling_rate_ms: config.eth_new_dr_polling_rate_ms,
-            skip_first: config.skip_first.unwrap_or(0),
+            polling_rate_ms: config.eth_new_drs_polling_rate_ms,
+            skip_first: config.storage_skip_first.unwrap_or(0),
+            max_batch_size: config.eth_max_batch_size,
         }
     }
 
     fn check_new_requests_from_ethereum(&self, ctx: &mut Context<Self>, period: Duration) {
-        log::debug!("Checking new DRs from Ethereum contract...");
+        log::debug!("Checking posted queries on the WitnetOracle contract...");
 
         let wrb_contract = self.wrb_contract.clone().unwrap();
         let skip_first = U256::from(self.skip_first);
+        let max_batch_size = self.max_batch_size;
+                
         // Check requests
         let fut = async move {
-            let total_requests_count: Result<U256, web3::contract::Error> = wrb_contract
+            let dr_database_addr = DrDatabase::from_registry();
+
+            let next_dr_id: Result<U256, web3::contract::Error> = wrb_contract
                 .query(
                     "getNextQueryId",
                     (),
@@ -90,73 +101,94 @@ impl EthPoller {
                     err
                 });
 
-            let dr_database_addr = DrDatabase::from_registry();
-            let db_request_count = dr_database_addr.send(GetLastDrId).await;
+            let last_dr_id = dr_database_addr.send(GetLastDrId).await;
 
-            if let (Ok(total_requests_count), Ok(Ok(mut db_request_count))) =
-                (total_requests_count, db_request_count)
+            if let (Ok(mut next_dr_id), Ok(Ok(mut last_dr_id))) =
+                (next_dr_id, last_dr_id)
             {
-                if db_request_count < skip_first {
+                if last_dr_id < skip_first {
                     log::debug!(
-                        "Skipping first {} requests per skip_first config param",
+                        "Skipping first {} queries as per SKIP_FIRST config param",
                         skip_first
                     );
-                    db_request_count = skip_first;
+                    last_dr_id = skip_first;
                 }
-                if db_request_count < total_requests_count {
-                    let init_index = usize::try_from(db_request_count + 1).unwrap();
-                    let last_index = usize::try_from(total_requests_count).unwrap();
+                if last_dr_id < next_dr_id {
+                    if last_dr_id + max_batch_size > next_dr_id {
+                        next_dr_id = last_dr_id + max_batch_size;
+                    }
+                    let init_index = usize::try_from(last_dr_id + 1).unwrap();
+                    let last_index = usize::try_from(next_dr_id).unwrap();
+                    let ids = init_index .. last_index;
+                    let ids: Vec<Token> = ids.map(|id| { Token::Uint(id.into()) }).collect();
 
-                    for i in init_index..last_index {
-                        log::debug!("[{}] checking dr in wrb", i);
-
-                        let query_status: Result<u8, web3::contract::Error> = wrb_contract
-                            .query(
-                                "getQueryStatus",
-                                (U256::from(i),),
-                                None,
-                                contract::Options::default(),
-                                None,
-                            )
-                            .await;
-
-                        match query_status {
-                            Ok(status) => match WitnetQueryStatus::from_code(status) {
+                    let queries_status: Result<Vec<u8>, web3::contract::Error> = wrb_contract
+                        .query(
+                            "getQueryStatusBatch",
+                            Token::Tuple(ids.clone()),
+                            None,
+                            contract::Options::default(),
+                            None
+                        )
+                        .await;
+                    
+                    if let Ok(queries_status) = queries_status {
+                        let mut posted_ids: Vec<Token> = vec![];
+                        for (pos, status) in queries_status.iter().enumerate() {
+                            let query_id = ids[pos].to_owned().into_uint().unwrap();
+                            match WitnetQueryStatus::from_code(*status) {
                                 WitnetQueryStatus::Unknown => {
-                                    log::debug!("[{}] has not exist, skipping", i)
+                                    log::debug!("[{}]: not available.", query_id);
                                 }
                                 WitnetQueryStatus::Posted => {
-                                    log::info!("[{}] new dr in wrb", i);
-                                    if let Ok(set_dr_info_bridge) =
-                                        process_posted_request(i.into(), &wrb_contract).await
-                                    {
-                                        dr_database_addr.do_send(set_dr_info_bridge);
-                                    } else {
-                                        break;
-                                    }
+                                    log::info!("[{}]: not yet reported.", query_id);
+                                    posted_ids.push(Token::Uint(query_id));
                                 }
-                                WitnetQueryStatus::Reported => {
-                                    log::debug!("[{}] already reported", i);
-                                    if let Ok(set_dr_info_bridge) =
-                                        process_posted_request(i.into(), &wrb_contract).await
-                                    {
-                                        dr_database_addr.do_send(set_dr_info_bridge);
-                                    } else {
-                                        break;
-                                    }
+                                WitnetQueryStatus::Reported
+                                    | WitnetQueryStatus::Finalized
+                                => {
+                                    log::debug!("[{}]: already reported.", query_id);
+                                    dr_database_addr.do_send(SetDrState { 
+                                        dr_id: query_id,
+                                        dr_state: DrState::Finished,
+                                    });
                                 }
-                                WitnetQueryStatus::Deleted => {
-                                    log::debug!("[{}] has been deleted, skipping", i)
-                                }
-                            },
-                            Err(err) => {
-                                log::error!(
-                                    "Fail to read getQueryStatus from contract: {:?}",
-                                    err.to_string(),
-                                );
-                                break;
                             }
                         }
+                        if !posted_ids.is_empty() {
+                            let dr_bytecodes: Result<Vec<Bytes>, web3::contract::Error> = wrb_contract
+                                .query(
+                                    "extractWitnetDataRequests",
+                                    Token::Tuple(posted_ids.clone()),
+                                    None,
+                                    contract::Options::default(),
+                                    None
+                                )
+                                .await;
+                            if let Ok(dr_bytecodes) = dr_bytecodes {
+                                for (pos, dr_id) in posted_ids.iter().enumerate() {
+                                    dr_database_addr.do_send(SetDrInfoBridge(
+                                        dr_id.to_owned().into_uint().unwrap(),
+                                        DrInfoBridge {
+                                            dr_bytes: dr_bytecodes[pos].to_owned(),
+                                            ..Default::default()
+                                        }
+                                    ));
+                                }
+                            } else {
+                                log::error!(
+                                    "Fail to extract Witnet Data Request bytecodes from queries {:?}",
+                                    posted_ids
+                                );
+                            }
+                        }
+                    } else {
+                        log::error!(
+                            "Fail to get status of queries #{} to #{}: {}",
+                             init_index, 
+                             last_index, 
+                             queries_status.unwrap_err().to_string()
+                        );
                     }
                 }
             }
@@ -173,49 +205,4 @@ impl EthPoller {
             actix::fut::ready(())
         }));
     }
-}
-
-/// Auxiliary function that process the information of a new posted request
-async fn process_posted_request(
-    query_id: U256,
-    wrb_contract: &Contract<web3::transports::Http>,
-) -> Result<SetDrInfoBridge, web3::contract::Error> {
-    let dr_bytes: Result<Bytes, web3::contract::Error> = wrb_contract
-        .query(
-            "readRequestBytecode",
-            (query_id,),
-            None,
-            contract::Options::default(),
-            None,
-        )
-        .await;
-
-    // Re-route some errors as success (explanation below)
-    match dr_bytes {
-        Ok(dr_bytes) => Ok(dr_bytes),
-        Err(err) => {
-            log::error!("Fail to read dr bytes from contract: {}", err.to_string());
-
-            // In some versions of the bridge contracts (those based on
-            // `WitnetRequestBoardTrustableBase`), we may get a revert when trying to fetch the dr
-            // bytes for a deleted query.
-            // If that's the case, we can return a success here, with empty bytes, so that the
-            // request can locally marked as complete, and we can move on.
-            if err.to_string().contains("WitnetRequestBoardTrustableBase") {
-                log::error!("Wait! This is an instance of `WitnetRequestBoardTrustableBase`. Let's assume we got a revert because the dr bytes were deleted, and simply move on.");
-
-                Ok(Default::default())
-            // Otherwise, handle the error normally
-            } else {
-                Err(err)
-            }
-        }
-    // Wrap the dr bytes in a `SetDrInfoBridge` structure
-    }.map(|dr_bytes| SetDrInfoBridge(
-        query_id,
-        DrInfoBridge {
-            dr_bytes,
-            ..Default::default()
-        },
-    ))
 }
