0c72d7f483fa8b3331fe3cf242c81023f0a472d0
Guillermo Díaz <guillermo@otherplane.com>
2024-09-24 14:31:06 +0200

    chore: cargo clippy --fix

--- bridges/centralized-ethereum/src/actors/dr_sender/tests.rs
+++ bridges/centralized-ethereum/src/actors/dr_sender/tests.rs
@@ -81,6 +81,7 @@ fn deserialize_dr_collateral_one_nanowit() {
 
     let dro_bytes = dro.to_pb_bytes().unwrap();
     let err = deserialize_and_validate_dr_bytes(&dro_bytes, 1, total_value).unwrap_err();
+    let err = deserialize_and_validate_dr_bytes(&dro_bytes, total_value, 1).unwrap_err();
     assert_eq!(err.encode_cbor(), vec![216, 39, 129, 24, 224]);
 }
 
