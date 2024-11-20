e82c5540c0481511b0f702bc386d8da9cf2b7767
aeweda <mraeweda@gmail.com>
2024-11-20 12:35:45 +0400

    ci: add macos openssl compilation script

---
+++ docker/cross-compilation/openssl-macos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -ex
+
+main() {
+    local version=1.0.2p
+    local install_dir="/usr/local/openssl"
+
+    # Install dependencies using brew
+    brew install \
+        m4 \
+        make
+
+    # Create temporary directory
+    td=$(mktemp -d)
+    pushd $td
+
+    # Download and extract OpenSSL
+    wget https://www.openssl.org/source/openssl-$version.tar.gz
+    tar --strip-components=1 -xzvf openssl-$version.tar.gz
+
+    # Configure OpenSSL for arm64, disabling assembly
+    ./Configure \
+        --prefix="$install_dir" \
+        darwin64-x86_64-cc \
+        no-dso \
+        no-asm \
+        -fPIC \
+        ${@:1}
+
+    # Modify the generated Makefile to use arm64
+    perl -pi -e 's/-arch x86_64/-arch arm64/g' Makefile
+
+    # Build using all available cores
+    KERNEL_BITS=64 make -j$(sysctl -n hw.ncpu)
+
+    # Install
+    sudo make install
+
+    popd
+
+    # Cleanup
+    rm -rf $td
+}
+
+main "${@}"
