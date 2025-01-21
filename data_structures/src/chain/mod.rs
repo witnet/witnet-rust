926616e919b01ddf63f8f04735ac0689e78e4aa0
drcpu <drcpu@protonmail.com>
2025-01-21 15:17:48 +0100

    chore(config): set testnet parameters for rc.9

--- data_structures/src/chain/mod.rs
+++ data_structures/src/chain/mod.rs
@@ -302,7 +302,7 @@ impl ConsensusConstantsWit2 {
     pub fn get_wit2_activation_delay_epochs(self) -> u32 {
         match get_environment() {
             Environment::Development | Environment::Testnet => {
-                1_920 // 24 hours
+                160 // 2 hours
             }
             _ => {
                 13_440 // 1 week
