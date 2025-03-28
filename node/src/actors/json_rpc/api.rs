2d5b27294642ecade34733444cb28496de581455
Guillermo Díaz <guillermo@otherplane.com>
2025-03-28 12:00:03 +0100

    chore: attend review comments

--- node/src/actors/json_rpc/api.rs
+++ node/src/actors/json_rpc/api.rs
@@ -106,9 +106,6 @@ pub fn attach_regular_methods<H>(
     server.add_actix_method(system, "getBalance2", |params: Params| {
         Box::pin(get_balance_2(params.parse()))
     });
-    server.add_actix_method(system, "getBalance2", |params: Params| {
-        Box::pin(get_balance_2(params.parse()))
-    });
     server.add_actix_method(system, "getReputation", |params: Params| {
         Box::pin(get_reputation(params.parse(), false))
     });
