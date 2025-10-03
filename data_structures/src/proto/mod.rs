f81fe7f396dc5083fe14d38ddf066a3034ac1fa5
drcpu <drcpu@protonmail.com>
2025-10-03 19:25:56 +0200

    fix(data_structures): remove unknown lint

--- data_structures/src/proto/mod.rs
+++ data_structures/src/proto/mod.rs
@@ -9,7 +9,6 @@ use protobuf::Message;
 use std::{convert::TryFrom, fmt::Debug};
 
 #[allow(elided_lifetimes_in_paths)]
-#[allow(mismatched_lifetime_syntaxes)]
 #[allow(renamed_and_removed_lints)]
 pub mod schema;
 pub mod versioning;
